54653050b8a9967f70906c1b0824866e674c3901
Witnesscm <fanmicao@126.com>
2021-02-04 22:56:47 +0800

    fix name text color.

--- Modules/UnitFrames/Core.lua
+++ Modules/UnitFrames/Core.lua
@@ -4,32 +4,42 @@ local oUF = _G.oUF or ns.oUF
 local UF = P:RegisterModule("UnitFrames")
 local NUF = B:GetModule("UnitFrames")
 
-function UF:UpdateNameText()
-	for _, frame in pairs(oUF.objects) do
-		local name = frame.nameText
-		local mystyle = frame.mystyle
-		local colorStr = UF.db["NameColor"] and frame.Health.colorClass and "" or "[color]"
-
-		if name then
-			if mystyle == "player" then
-				frame:Tag(name, " "..colorStr.."[name]")
-			elseif mystyle == "target" then
-				frame:Tag(name, "[fulllevel] "..colorStr.."[name][afkdnd]")
-			elseif mystyle == "focus" then
-				frame:Tag(name, colorStr.."[name][afkdnd]")
-			elseif mystyle == "arena" then
-				frame:Tag(name, "[arenaspec] "..colorStr.."[name]")
-			elseif mystyle == "raid" and C.db["UFs"]["SimpleMode"] and C.db["UFs"]["ShowTeamIndex"] and not frame.isPartyPet and not frame.isPartyFrame then
-				frame:Tag(name, "[group]."..colorStr.."[name]")
-			else
-				frame:Tag(name, colorStr.."[name]")
-			end
-			name:UpdateTag()
+function UF:SetTag(frame)
+	local name = frame.nameText
+	local mystyle = frame.mystyle
+	local colorStr = UF.db["NameColor"] and frame.Health.colorClass and "" or "[color]"
+	if name then
+		if mystyle == "player" then
+			frame:Tag(name, " "..colorStr.."[name]")
+		elseif mystyle == "target" then
+			frame:Tag(name, "[fulllevel] "..colorStr.."[name][afkdnd]")
+		elseif mystyle == "focus" then
+			frame:Tag(name, colorStr.."[name][afkdnd]")
+		elseif mystyle == "arena" then
+			frame:Tag(name, "[arenaspec] "..colorStr.."[name]")
+		elseif mystyle == "raid" and C.db["UFs"]["SimpleMode"] and C.db["UFs"]["ShowTeamIndex"] and not frame.isPartyPet and not frame.isPartyFrame then
+			frame:Tag(name, "[group]."..colorStr.."[name]")
+		else
+			frame:Tag(name, colorStr.."[name]")
 		end
+		name:UpdateTag()
 	end
 end
 
+function UF:UpdateNameText()
+	for _, frame in pairs(oUF.objects) do
+		UF:SetTag(frame)
+	end
+end
+
+function UF:SetNameTextHook()
+	hooksecurefunc(NUF, "CreateHealthText", function(self, frame)
+		UF:SetTag(frame)
+	end)
+end
+
 function UF:SetupNameText()
+	UF:SetNameTextHook()
 	UF:UpdateNameText()
 	hooksecurefunc(NUF, "UpdateTextScale", UF.UpdateNameText)
 	hooksecurefunc(NUF, "UpdateRaidTextScale", UF.UpdateNameText)
