681932e48c7e2fddc56a735709177b07979a40a4
Witnesscm <fanmicao@126.com>
2021-03-13 20:54:25 +0800

    Update Media

---
+++ Media/SharedMedia.lua
@@ -0,0 +1,27 @@
+local _, ns = ...
+local B, C, L, DB, P = unpack(ns)
+---------------------------------
+-- Credit: WindMedia, by houshuu
+---------------------------------
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- 风行丨GG @ NGA
+LSM:Register("statusbar", "FXGG_1", [[Interface\Addons\NDui_Plus\Media\Statusbar\FX_001.tga]])
+LSM:Register("statusbar", "FXGG_2", [[Interface\Addons\NDui_Plus\Media\Statusbar\FX_002.tga]])
+LSM:Register("statusbar", "FXGG_3", [[Interface\Addons\NDui_Plus\Media\Statusbar\FX_003.tga]])
+LSM:Register("statusbar", "FXGG_4", [[Interface\Addons\NDui_Plus\Media\Statusbar\FX_004.tga]])
+-- houshuu
+LSM:Register("statusbar", "Wind_1", [[Interface\Addons\NDui_Plus\Media\Statusbar\WindTools_1.tga]])
+LSM:Register("statusbar", "Wind_2", [[Interface\Addons\NDui_Plus\Media\Statusbar\WindTools_2.tga]])
+-- ElvUI
+LSM:Register("statusbar", "ElvUI_1", [[Interface\Addons\NDui_Plus\Media\Statusbar\ElvUI_1.tga]])
+LSM:Register("statusbar", "ElvUI_2", [[Interface\Addons\NDui_Plus\Media\Statusbar\ElvUI_2.tga]])
+-- ElvUI_MerathilisUI
+LSM:Register("statusbar", "mUI_1", [[Interface\Addons\NDui_Plus\Media\Statusbar\mUI_1.tga]])
+LSM:Register("statusbar", "mUI_2", [[Interface\Addons\NDui_Plus\Media\Statusbar\mUI_2.tga]])
+LSM:Register("statusbar", "mUI_3", [[Interface\Addons\NDui_Plus\Media\Statusbar\mUI_3.tga]])
+LSM:Register("statusbar", "mUI_4", [[Interface\Addons\NDui_Plus\Media\Statusbar\mUI_4.tga]])
+LSM:Register("statusbar", "mUI_5", [[Interface\Addons\NDui_Plus\Media\Statusbar\mUI_5.tga]])
+LSM:Register("statusbar", "mUI_6", [[Interface\Addons\NDui_Plus\Media\Statusbar\mUI_6.tga]])
+LSM:Register("statusbar", "mUI_7", [[Interface\Addons\NDui_Plus\Media\Statusbar\mUI_7.tga]])
+LSM:Register("statusbar", "mUI_8", [[Interface\Addons\NDui_Plus\Media\Statusbar\mUI_8.tga]])
