fcfec0b62d01e39422fa5e810174d75554a5ee0a
unknown <fanmicao@126.com>
2021-08-12 17:19:27 +0800

    Korthia daily quest

--- Modules/Quest/Training.lua
+++ Modules/Quest/Training.lua
@@ -1,9 +1,10 @@
+local _, ns = ...
+local B, C, L, DB, P = unpack(ns)
 -----------------------
 -- BetterWorldQuests
 -- Author: p3lim
 -----------------------
-local MESSAGE = 'Stand in circle and spam <SpaceBar> to complete!'
-local BUTTON = 'OverrideActionBarButton%d'
+local BUTTON = "OverrideActionBarButton%d"
 
 local actionMessages = {}
 local actionResetSpells = {}
@@ -23,105 +24,125 @@ local spells = {
 		[342001] = 2, -- Kick
 		[342002] = 3, -- Dodge
 	},
+	[355677] = {
+		[355834] = 1, -- 突刺
+		[355835] = 2, -- 招架
+		[355836] = 3, -- 还击
+	},
 }
 
 local locale = GetLocale()
-local trainerName = 'Trainer Ikaros'
-if locale == 'deDE' then
-	trainerName = 'Ausbilder Ikaros'
-elseif locale == 'esES' or locale == 'esMX' then
-	trainerName = 'Instructor Ikaros'
-elseif locale == 'frFR' then
-	trainerName = 'Instructeur Ikaros'
-elseif locale == 'itIT' then
-	trainerName = 'Istruttore Ikaros'
-elseif locale == 'koKR' then
-	trainerName = '훈련사 이카로스'
-elseif locale == 'ptBR' then
-	trainerName = 'Treinador Ikaros'
-elseif locale == 'ruRU' then
-	trainerName = 'Укротитель Икар'
-elseif locale == 'zhCN' or locale == 'zhTW' then
-	trainerName = '训练师伊卡洛斯'
-	MESSAGE = '站在圈里狂按 <空格> 完成!'
+local MESSAGE = "Stand in circle and spam <SpaceBar> to complete!"
+local trainerName = "Trainer Ikaros"
+local nadjiaName = "Nadjia the Mistblade"
+
+if locale == "deDE" then
+	trainerName = "Ausbilder Ikaros"
+elseif locale == "esES" or locale == "esMX" then
+	trainerName = "Instructor Ikaros"
+elseif locale == "frFR" then
+	trainerName = "Instructeur Ikaros"
+elseif locale == "itIT" then
+	trainerName = "Istruttore Ikaros"
+elseif locale == "koKR" then
+	trainerName = "훈련사 이카로스"
+elseif locale == "ptBR" then
+	trainerName = "Treinador Ikaros"
+elseif locale == "ruRU" then
+	trainerName = "Укротитель Икар"
+elseif locale == "zhCN" or locale == "zhTW" then
+	MESSAGE = "站在圈里狂按 <空格> 完成!"
+	trainerName = "训练师伊卡洛斯"
+	nadjiaName = "娜德佳，迷雾之刃"
 end
 
-local function GetNPCIDByGUID(guid)
-	return guid and (tonumber((string.match(guid, 'Creature%-.-%-.-%-.-%-.-%-(.-)%-')) or ''))
-end
+local questIDs = {
+	[59585] = true,
+	[64271] = true,
+}
 
-local function auraFilter(a, _, _, _, _, _, _, _, _, _, _, _, b)
-	return a == b
-end
+local questNPCs = {
+	[trainerName] = true,
+	[nadjiaName] = true,
+}
 
-local Handler = CreateFrame('Frame')
-Handler:RegisterEvent('QUEST_LOG_UPDATE')
-Handler:RegisterEvent('QUEST_ACCEPTED')
-Handler:SetScript('OnEvent', function(self, event, ...)
+local Handler = CreateFrame("Frame")
+Handler:RegisterEvent("QUEST_LOG_UPDATE")
+Handler:RegisterEvent("QUEST_ACCEPTED")
+Handler:SetScript("OnEvent", function(self, event, ...)
 	if IsAddOnLoaded("BetterWorldQuests") then return end
 	if not NDuiPlusDB["Misc"]["QuestHelper"] then return end
 
-	if event == 'QUEST_LOG_UPDATE' then
-		if C_QuestLog.IsOnQuest(59585) then
+	if event == "QUEST_LOG_UPDATE" then
+		local found = false
+
+		for questID in pairs(questIDs) do
+			if C_QuestLog.IsOnQuest(questID) then
+				found = true
+				break
+			end
+		end
+
+		if found then
 			self:Watch()
 		else
 			self:Unwatch()
 		end
-	elseif event == 'QUEST_ACCEPTED' then
+	elseif event == "QUEST_ACCEPTED" then
 		local questID = ...
-		if questID == 59585 then
+		if questIDs[questID] then
 			self:Watch()
 		end
-	elseif(event == 'QUEST_REMOVED') then
+	elseif event == "QUEST_REMOVED" then
 		local questID = ...
-		if questID == 59585 then
+		if questIDs[questID] then
 			self:Unwatch()
 		end
-	elseif event == 'UNIT_AURA' then
+	elseif event == "UNIT_AURA" then
 		for buff, spellSet in next, spells do
-			if AuraUtil.FindAura(auraFilter, 'player', 'HELPFUL', buff) then
+			if GetPlayerAuraBySpellID(buff) then
 				self:Control(spellSet)
 				return
 			else
 				self:Uncontrol()
 			end
 		end
-	elseif event == 'CHAT_MSG_MONSTER_SAY' then
+	elseif event == "CHAT_MSG_MONSTER_SAY" then
 		local msg, sender = ...
-		if sender == trainerName then -- Trainer Ikaros
+		if questNPCs[sender] then
 			for spell, actionID in pairs (actionMessages) do
 				if strmatch(msg, spell) then
-					C_Timer.After(0.5, function()
+					C_Timer.After(.2, function()
 						-- wait a split second to get "Perfect"
 						ClearOverrideBindings(self)
-						SetOverrideBindingClick(self, true, 'SPACE', BUTTON:format(actionID))
+						SetOverrideBindingClick(self, true, "SPACE", BUTTON:format(actionID))
 					end)
 					break
 				end
 			end
 		end
-	elseif event == 'UNIT_SPELLCAST_SUCCEEDED' then
+	elseif event == "UNIT_SPELLCAST_SUCCEEDED" then
 		local _, _, spellID = ...
 		if actionResetSpells[spellID] then
 			ClearOverrideBindings(self)
 
 			-- bind to something useless to avoid spamming jump
-			SetOverrideBindingClick(self, true, 'SPACE', BUTTON:format(12))
+			SetOverrideBindingClick(self, true, "SPACE", BUTTON:format(12))
 		end
-	elseif(event == 'PLAYER_REGEN_ENABLED') then
+	elseif(event == "PLAYER_REGEN_ENABLED") then
 		ClearOverrideBindings(self)
 		self:UnregisterEvent(event)
 	end
 end)
 
 function Handler:Watch()
-	self:RegisterUnitEvent('UNIT_AURA', 'player')
-	self:RegisterEvent('QUEST_REMOVED')
+	self:RegisterUnitEvent("UNIT_AURA", "player")
+	self:RegisterEvent("QUEST_REMOVED")
 end
 
 function Handler:Unwatch()
-	self:UnregisterEvent('UNIT_AURA')
-	self:UnregisterEvent('QUEST_REMOVED')
+	self:UnregisterEvent("UNIT_AURA")
+	self:UnregisterEvent("QUEST_REMOVED")
 	self:Uncontrol()
 end
 
@@ -132,26 +153,29 @@ function Handler:Control(spellSet)
 		actionMessages[(GetSpellInfo(spellID))] = actionIndex
 		actionResetSpells[spellID] = true
 
+		-- zhCN fix
 		if spellID == 321844 then
-			actionMessages["低扫"] = actionIndex -- zhCN fix
+			actionMessages["低扫"] = actionIndex
+		elseif spellID == 355834 then
+			actionMessages["突袭"] = actionIndex
 		end
 	end
 
 	-- bind to something useless to avoid spamming jump
-	SetOverrideBindingClick(self, true, 'SPACE', BUTTON:format(12))
+	SetOverrideBindingClick(self, true, "SPACE", BUTTON:format(12))
 
 	self:Message()
 
-	self:RegisterUnitEvent('UNIT_SPELLCAST_SUCCEEDED', 'player')
-	self:RegisterEvent('CHAT_MSG_MONSTER_SAY')
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "player")
+	self:RegisterEvent("CHAT_MSG_MONSTER_SAY")
 end
 
 function Handler:Uncontrol()
-	self:UnregisterEvent('UNIT_SPELLCAST_SUCCEEDED')
-	self:UnregisterEvent('CHAT_MSG_MONSTER_SAY')
+	self:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+	self:UnregisterEvent("CHAT_MSG_MONSTER_SAY")
 
 	if InCombatLockdown() then
-		self:RegisterEvent('PLAYER_REGEN_ENABLED')
+		self:RegisterEvent("PLAYER_REGEN_ENABLED")
 	else
 		ClearOverrideBindings(self)
 	end
