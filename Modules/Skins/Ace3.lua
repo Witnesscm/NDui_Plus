5b2e736663ef162ac5fae2be306defceaa940aa3
unknown <fanmicao@126.com>
2021-09-17 22:39:57 +0800

    Update skin for Ace3

--- Modules/Skins/Ace3.lua
+++ Modules/Skins/Ace3.lua
@@ -8,20 +8,15 @@ local _G = getfenv(0)
 local select, pairs, type = select, pairs, type
 local r, g, b = DB.r, DB.g, DB.b
 local TT = B:GetModule("Tooltip")
-S.EarlyAceWidgets = {}
+
+-- versions of AceGUI and AceConfigDialog.
+local minorGUI, minorConfigDialog = 36, 76
 
 function S:Ace3()
-	local AceGUI = LibStub and LibStub('AceGUI-3.0', true)
+	local AceGUI = LibStub and LibStub("AceGUI-3.0", true)
 
 	if not AceGUI then return end
 	if not S.db["Ace3"] then return end
-
-	if AceGUITooltip then
-		AceGUITooltip:HookScript("OnShow", TT.ReskinTooltip)
-	end
-	if AceConfigDialogTooltip then
-		AceConfigDialogTooltip:HookScript("OnShow", TT.ReskinTooltip)
-	end
 
 	for _, n in next, S.EarlyAceWidgets do
 		if n.SetLayout then
@@ -29,6 +24,10 @@ function S:Ace3()
 		else
 			S:Ace3_RegisterAsWidget(n)
 		end
+	end
+
+	for _, n in next, S.EarlyAceTooltips do
+		S:Ace3_SkinTooltip(_G.LibStub(n, true))
 	end
 end
 
@@ -52,13 +51,13 @@ end
 function S:Ace3_SkinTab(tab)
 	B.StripTextures(tab)
 	tab.bg = B.CreateBDFrame(tab)
-	tab.bg:SetPoint('TOPLEFT', 8, -3)
-	tab.bg:SetPoint('BOTTOMRIGHT', -8, 0)
+	tab.bg:SetPoint("TOPLEFT", 8, -3)
+	tab.bg:SetPoint("BOTTOMRIGHT", -8, 0)
 	tab.text:SetPoint("LEFT", 14, -1)
 
 	tab:HookScript("OnEnter", B.Texture_OnEnter)
 	tab:HookScript("OnLeave", B.Texture_OnLeave)
-	hooksecurefunc(tab, 'SetSelected', function(self, selected)
+	hooksecurefunc(tab, "SetSelected", function(self, selected)
 		if selected then
 			self.bg:SetBackdropColor(r, g, b, .25)
 		else
@@ -91,7 +90,7 @@ end
 
 function S:Ace3_RegisterAsWidget(widget)
 	local TYPE = widget.type
-	if TYPE == 'MultiLineEditBox' then
+	if TYPE == "MultiLineEditBox" then
 		B.StripTextures(widget.scrollBG)
 		local bg = B.CreateBDFrame(widget.scrollBG, .8)
 		bg:SetPoint("TOPLEFT", 0, -2)
@@ -99,11 +98,11 @@ function S:Ace3_RegisterAsWidget(widget)
 		B.Reskin(widget.button)
 		B.ReskinScroll(widget.scrollBar)
 
-		widget.scrollBar:SetPoint('RIGHT', widget.frame, 'RIGHT', 0 -4)
-		widget.scrollBG:SetPoint('TOPRIGHT', widget.scrollBar, 'TOPLEFT', -2, 19)
-		widget.scrollBG:SetPoint('BOTTOMLEFT', widget.button, 'TOPLEFT')
-		widget.scrollFrame:SetPoint('BOTTOMRIGHT', widget.scrollBG, 'BOTTOMRIGHT', -4, 8)
-	elseif TYPE == 'CheckBox' then
+		widget.scrollBar:SetPoint("RIGHT", widget.frame, "RIGHT", 0 -4)
+		widget.scrollBG:SetPoint("TOPRIGHT", widget.scrollBar, "TOPLEFT", -2, 19)
+		widget.scrollBG:SetPoint("BOTTOMLEFT", widget.button, "TOPLEFT")
+		widget.scrollFrame:SetPoint("BOTTOMRIGHT", widget.scrollBG, "BOTTOMRIGHT", -4, 8)
+	elseif TYPE == "CheckBox" then
 		local check = widget.check
 		local checkbg = widget.checkbg
 		local highlight = widget.highlight
@@ -128,7 +127,7 @@ function S:Ace3_RegisterAsWidget(widget)
 		check:SetVertexColor(r, g, b)
 		check.SetDesaturated = B.Dummy
 
-		hooksecurefunc(widget, 'SetDisabled', function(self, disabled)
+		hooksecurefunc(widget, "SetDisabled", function(self, disabled)
 			local check = self.check
 			if disabled then
 				check:SetVertexColor(.8, .8, .8)
@@ -145,7 +144,7 @@ function S:Ace3_RegisterAsWidget(widget)
 				self.check:SetAllPoints(self.checkbg)
 			end
 		end)
-	elseif TYPE == 'Dropdown' or TYPE == 'LQDropdown' then
+	elseif TYPE == "Dropdown" or TYPE == "LQDropdown" then
 		local frame = widget.dropdown
 		local button = widget.button
 		local button_cover = widget.button_cover
@@ -158,20 +157,20 @@ function S:Ace3_RegisterAsWidget(widget)
 		B.CreateGradient(bg)
 
 		widget.label:ClearAllPoints()
-		widget.label:SetPoint('BOTTOMLEFT', bg, 'TOPLEFT', 2, 0)
+		widget.label:SetPoint("BOTTOMLEFT", bg, "TOPLEFT", 2, 0)
 
 		B.ReskinArrow(button, "down")
 		button:SetSize(20, 20)
 		button:ClearAllPoints()
-		button:SetPoint('RIGHT', bg)
+		button:SetPoint("RIGHT", bg)
 
 		text:ClearAllPoints()
 		text:SetJustifyH("RIGHT")
-		text:SetPoint('RIGHT', button, 'LEFT', -3, 0)
+		text:SetPoint("RIGHT", button, "LEFT", -3, 0)
 
-		button:HookScript('OnClick', S.Ace3_SkinDropdown)
-		button_cover:HookScript('OnClick', S.Ace3_SkinDropdown)
-	elseif TYPE == 'LSM30_Font' or TYPE == 'LSM30_Sound' or TYPE == 'LSM30_Border' or TYPE == 'LSM30_Background' or TYPE == 'LSM30_Statusbar' then
+		button:HookScript("OnClick", S.Ace3_SkinDropdown)
+		button_cover:HookScript("OnClick", S.Ace3_SkinDropdown)
+	elseif TYPE == "LSM30_Font" or TYPE == "LSM30_Sound" or TYPE == "LSM30_Border" or TYPE == "LSM30_Background" or TYPE == "LSM30_Statusbar" then
 		local frame = widget.frame
 		local button = frame.dropButton
 		local text = frame.text
@@ -183,33 +182,33 @@ function S:Ace3_RegisterAsWidget(widget)
 		B.CreateGradient(bg)
 
 		frame.label:ClearAllPoints()
-		frame.label:SetPoint('BOTTOMLEFT', bg, 'TOPLEFT', 2, 0)
+		frame.label:SetPoint("BOTTOMLEFT", bg, "TOPLEFT", 2, 0)
 
 		B.ReskinArrow(button, "down")
 		button:SetSize(20, 20)
 		button:ClearAllPoints()
-		button:SetPoint('RIGHT', bg)
+		button:SetPoint("RIGHT", bg)
 
 		frame.text:ClearAllPoints()
-		frame.text:SetPoint('RIGHT', button, 'LEFT', -2, 0)
+		frame.text:SetPoint("RIGHT", button, "LEFT", -2, 0)
 
-		if TYPE == 'LSM30_Sound' then
+		if TYPE == "LSM30_Sound" then
 			widget.soundbutton:SetParent(bg)
 			widget.soundbutton:ClearAllPoints()
-			widget.soundbutton:SetPoint('LEFT', bg, 'LEFT', 2, 0)
-		elseif TYPE == 'LSM30_Statusbar' then
+			widget.soundbutton:SetPoint("LEFT", bg, "LEFT", 2, 0)
+		elseif TYPE == "LSM30_Statusbar" then
 			widget.bar:SetParent(bg)
 			widget.bar:ClearAllPoints()
-			widget.bar:SetPoint('TOPLEFT', bg, 'TOPLEFT', 2, -2)
-			widget.bar:SetPoint('BOTTOMRIGHT', button, 'BOTTOMLEFT', -1, 0)
-		elseif TYPE == 'LSM30_Border' or TYPE == 'LSM30_Background' then
+			widget.bar:SetPoint("TOPLEFT", bg, "TOPLEFT", 2, -2)
+			widget.bar:SetPoint("BOTTOMRIGHT", button, "BOTTOMLEFT", -1, 0)
+		elseif TYPE == "LSM30_Border" or TYPE == "LSM30_Background" then
 			bg:SetPoint("TOPLEFT", 45, -22)
 		end
 
 		button:SetParent(bg)
 		text:SetParent(bg)
-		button:HookScript('OnClick', S.Ace3_SkinDropdown)
-	elseif TYPE == 'EditBox' then
+		button:HookScript("OnClick", S.Ace3_SkinDropdown)
+	elseif TYPE == "EditBox" then
 		B.Reskin(widget.button)
 		P.ReskinInput(widget.editbox)
 		widget.editbox.bg:SetPoint("TOPLEFT", 0, -2)
@@ -220,14 +219,14 @@ function S:Ace3_RegisterAsWidget(widget)
 				self:SetPoint(a, b, c, 0, e)
 			end
 		end)
-	elseif TYPE == 'Button' or TYPE == 'MacroButton' then
+	elseif TYPE == "Button" or TYPE == "MacroButton" then
 		B.Reskin(widget.frame)
-	elseif TYPE == 'Slider' then
+	elseif TYPE == "Slider" then
 		B.ReskinSlider(widget.slider)
 		widget.editbox:SetBackdrop(nil)
 		B.ReskinInput(widget.editbox)
-		widget.editbox:SetPoint('TOP', widget.slider, 'BOTTOM', 0, -1)
-	elseif TYPE == 'Keybinding' then
+		widget.editbox:SetPoint("TOP", widget.slider, "BOTTOM", 0, -1)
+	elseif TYPE == "Keybinding" then
 		local button = widget.button
 		local msgframe = widget.msgframe
 
@@ -236,9 +235,9 @@ function S:Ace3_RegisterAsWidget(widget)
 		B.SetBD(msgframe)
 		msgframe.msg:ClearAllPoints()
 		msgframe.msg:SetPoint("CENTER")
-	elseif TYPE == 'Icon' then
+	elseif TYPE == "Icon" then
 		B.StripTextures(widget.frame)
-	elseif TYPE == 'WeakAurasDisplayButton' then
+	elseif TYPE == "WeakAurasDisplayButton" then
 		local button = widget.frame
 
 		P.ReskinCollapse(widget.expand)
@@ -258,19 +257,27 @@ function S:Ace3_RegisterAsWidget(widget)
 
 		hooksecurefunc(widget, "SetIcon", S.WeakAuras_SkinIcon)
 		hooksecurefunc(widget, "UpdateThumbnail", S.WeakAuras_UpdateIcon)
-	elseif TYPE == 'WeakAurasNewButton' then
+	elseif TYPE == "WeakAurasNewButton" then
 		local button = widget.frame
 
 		widget.icon:SetTexCoord(unpack(DB.TexCoord))
 		widget.icon:ClearAllPoints()
-		widget.icon:SetPoint("LEFT", widget.frame, "LEFT", 1, 0)
+		widget.icon:SetPoint("LEFT", button, "LEFT", 1, 0)
 		button.iconBG = B.CreateBDFrame(widget.icon, 0)
 		button.iconBG:SetAllPoints(widget.icon)
 
 		button.highlight:SetTexture(DB.bdTex)
 		button.highlight:SetVertexColor(DB.r, DB.g, DB.b, .25)
 		button.highlight:SetInside()
-	elseif TYPE == 'WeakAurasMultiLineEditBox' then
+	elseif TYPE == "WeakAurasPendingUpdateButton" then
+		local button = widget.frame
+
+		widget.icon:SetTexCoord(unpack(DB.TexCoord))
+		widget.icon:ClearAllPoints()
+		widget.icon:SetPoint("LEFT", button, "LEFT", 1, 0)
+		button.iconBG = B.CreateBDFrame(widget.icon, 0)
+		button.iconBG:SetAllPoints(widget.icon)
+	elseif TYPE == "WeakAurasMultiLineEditBox" then
 		B.StripTextures(widget.scrollBG)
 		local bg = B.CreateBDFrame(widget.scrollBG, .8)
 		bg:SetPoint("TOPLEFT", 0, -2)
@@ -278,10 +285,10 @@ function S:Ace3_RegisterAsWidget(widget)
 		B.Reskin(widget.button)
 		B.ReskinScroll(widget.scrollBar)
 
-		widget.scrollBar:SetPoint('RIGHT', widget.frame, 'RIGHT', 0 -4)
-		widget.scrollBG:SetPoint('TOPRIGHT', widget.scrollBar, 'TOPLEFT', -2, 19)
-		widget.scrollBG:SetPoint('BOTTOMLEFT', widget.button, 'TOPLEFT')
-		widget.scrollFrame:SetPoint('BOTTOMRIGHT', widget.scrollBG, 'BOTTOMRIGHT', -4, 8)
+		widget.scrollBar:SetPoint("RIGHT", widget.frame, "RIGHT", 0 -4)
+		widget.scrollBG:SetPoint("TOPRIGHT", widget.scrollBar, "TOPLEFT", -2, 19)
+		widget.scrollBG:SetPoint("BOTTOMLEFT", widget.button, "TOPLEFT")
+		widget.scrollFrame:SetPoint("BOTTOMRIGHT", widget.scrollBG, "BOTTOMRIGHT", -4, 8)
 
 		widget.frame:HookScript("OnShow", function()
 			if widget.extraButtons then
@@ -293,17 +300,17 @@ function S:Ace3_RegisterAsWidget(widget)
 				end
 			end
 		end)
-	elseif TYPE == 'WeakAurasLoadedHeaderButton' then
+	elseif TYPE == "WeakAurasLoadedHeaderButton" then
 		P.ReskinCollapse(widget.expand)
 		widget.expand:SetPushedTexture("")
 		widget.expand.SetPushedTexture = B.Dummy
-	elseif TYPE == 'WeakAurasIconButton' then
+	elseif TYPE == "WeakAurasIconButton" then
 		local bg = B.ReskinIcon(widget.texture)
 		bg:SetBackdropColor(0, 0, 0, 0)
 		local hl = widget.frame:GetHighlightTexture()
 		hl:SetColorTexture(1, 1, 1, .25)
 		hl:SetAllPoints()
-	elseif TYPE == 'WeakAurasTextureButton' then
+	elseif TYPE == "WeakAurasTextureButton" then
 		local button = widget.frame
 
 		B.CreateBD(button, .25)
@@ -316,16 +323,16 @@ end
 
 function S:Ace3_RegisterAsContainer(widget)
 	local TYPE = widget.type
-	if TYPE == 'ScrollFrame' then
+	if TYPE == "ScrollFrame" then
 		B.ReskinScroll(widget.scrollbar)
 		widget.scrollbar:DisableDrawLayer("BACKGROUND")
-	elseif TYPE == 'InlineGroup' or TYPE == 'TreeGroup' or TYPE == 'TabGroup' or TYPE == 'Frame' or TYPE == 'DropdownGroup' or TYPE == "Window" or TYPE == "WeakAurasTreeGroup" then
+	elseif TYPE == "InlineGroup" or TYPE == "TreeGroup" or TYPE == "TabGroup" or TYPE == "Frame" or TYPE == "DropdownGroup" or TYPE == "Window" or TYPE == "WeakAurasTreeGroup" then
 		local frame = widget.content:GetParent()
 		B.StripTextures(frame)
-		if TYPE == 'Frame' then
+		if TYPE == "Frame" then
 			for i = 1, frame:GetNumChildren() do
 				local child = select(i, frame:GetChildren())
-				if child:GetObjectType() == 'Button' and child:GetText() then
+				if child:GetObjectType() == "Button" and child:GetText() then
 					B.Reskin(child)
 				else
 					B.StripTextures(child)
@@ -337,7 +344,7 @@ function S:Ace3_RegisterAsContainer(widget)
 			frame.bg:SetPoint("TOPLEFT", 2, -2)
 			frame.bg:SetPoint("BOTTOMRIGHT", -2, 2)
 		end
-	
+
 		if TYPE == "Window" then
 			B.ReskinClose(frame.obj.closebutton)
 		end
@@ -368,7 +375,7 @@ function S:Ace3_RegisterAsContainer(widget)
 			end
 		end
 
-		if TYPE == 'TabGroup' then
+		if TYPE == "TabGroup" then
 			local oldCreateTab = widget.CreateTab
 			widget.CreateTab = function(self, id)
 				local tab = oldCreateTab(self, id)
@@ -400,14 +407,21 @@ function S:Ace3_MetaTable(lib)
 end
 
 function S:Ace3_MetaIndex(k, v)
-	if k == 'RegisterAsContainer' then
+	if k == "tooltip" then
+		rawset(self, k, v)
+		TT.ReskinTooltip(v)
+	elseif k == "popup" then
+		rawset(self, k, v)
+		v:HookScript("OnShow", S.Ace3_StylePopup)
+		v.IsHooked = true
+	elseif k == "RegisterAsContainer" then
 		rawset(self, k, function(s, w, ...)
 			if S.db["Ace3"] then
 				S.Ace3_RegisterAsContainer(s, w, ...)
 			end
 			return v(s, w, ...)
 		end)
-	elseif k == 'RegisterAsWidget' then
+	elseif k == "RegisterAsWidget" then
 		rawset(self, k, function(...)
 			if S.db["Ace3"] then
 				S.Ace3_RegisterAsWidget(...)
@@ -419,8 +433,43 @@ function S:Ace3_MetaIndex(k, v)
 	end
 end
 
--- versions of AceGUI and AceConfigDialog.
-local minorGUI, minorConfigDialog = 36, 76
+function S:Ace3_StylePopup()
+	if not self:IsForbidden() and not self.styled then
+		B.StripTextures(self)
+		B.SetBD(self)
+
+		for _, key in pairs({"accept", "cancel"}) do
+			local bu = self[key]
+			if bu then
+				B.Reskin(bu)
+			end
+		end
+
+		for _, child in pairs {self:GetChildren()} do
+			if child.Bg and child.layoutType and child.layoutType == "Dialog" then
+				B.StripTextures(child)
+				break
+			end
+		end
+
+		self.styled = true
+	end
+end
+
+function S:Ace3_SkinTooltip(lib, minor) -- lib: AceConfigDialog or AceGUI
+	if not lib or (minor and minor < minorConfigDialog) then return end
+
+	if not lib.tooltip then
+		S:Ace3_MetaTable(lib)
+	else
+		TT.ReskinTooltip(lib.tooltip)
+
+		if lib.popup and not lib.popup.IsHooked then -- StaticPopup
+			lib.popup:HookScript("OnShow", S.Ace3_StylePopup)
+		end
+	end
+end
+
 local lastMinor = 0
 function S:HookAce3(lib, minor, earlyLoad) -- lib: AceGUI
 	if not lib or (not minor or minor < minorGUI) then return end
@@ -447,24 +496,38 @@ function S:HookAce3(lib, minor, earlyLoad) -- lib: AceGUI
 	if earlyWidget then lib.RegisterAsWidget = earlyWidget end
 end
 
-
 do -- Early Skin Loading
 	local Libraries = {
-		['AceGUI'] = true,
+		["AceGUI"] = true,
+		["AceConfigDialog"] = true,
 	}
+
+	S.EarlyAceWidgets = {}
+	S.EarlyAceTooltips = {}
 
 	local LibStub = _G.LibStub
 	if not LibStub then return end
 
-	local numEnding = '%-[%d%.]+$'
+	local numEnding = "%-[%d%.]+$"
 	function S:LibStub_NewLib(major, minor)
-		local earlyLoad = major == 'ElvUI'
+		local earlyLoad = major == "ElvUI"
 		if earlyLoad then major = minor end
 
-		local n = gsub(major, numEnding, '')
+		local n = gsub(major, numEnding, "")
 		if Libraries[n] then
-			if n == 'AceGUI' then
+			if n == "AceGUI" then
 				S:HookAce3(LibStub.libs[major], LibStub.minors[major], earlyLoad)
+				if earlyLoad then
+					tinsert(S.EarlyAceTooltips, major)
+				else
+					S:Ace3_SkinTooltip(LibStub.libs[major])
+				end
+			elseif n == "AceConfigDialog" then
+				if earlyLoad then
+					tinsert(S.EarlyAceTooltips, major)
+				else
+					S:Ace3_SkinTooltip(LibStub.libs[major], LibStub.minors[major])
+				end
 			end
 		end
 	end
@@ -484,15 +547,15 @@ do -- Early Skin Loading
 	end
 
 	for n in next, LibStub.libs do
-		if n == 'AceGUI-3.0' then
+		if n == "AceGUI-3.0" then
 			for _, x in ipairs({_G.UIParent:GetChildren()}) do
 				if x and x.obj then earlyWidget(x.obj) end
 			end
 		end
-		if Libraries[gsub(n, numEnding, '')] then
-			S:LibStub_NewLib('ElvUI', n)
+		if Libraries[gsub(n, numEnding, "")] then
+			S:LibStub_NewLib("ElvUI", n)
 		end
 	end
 
-	hooksecurefunc(LibStub, 'NewLibrary', S.LibStub_NewLib)
+	hooksecurefunc(LibStub, "NewLibrary", S.LibStub_NewLib)
 end
