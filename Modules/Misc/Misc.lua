4dd7782430e139c8d8ab113392a765a105efe492
unknown <fanmicao@126.com>
2022-02-27 20:21:42 +0800

    Fix duplicate LFG applications after patch 9.1.5

--- Modules/Misc/Misc.lua
+++ Modules/Misc/Misc.lua
@@ -141,3 +141,17 @@ function M:ThreadsOfFateString()
 	end
 end
 P:AddCallbackForAddon("Blizzard_PlayerChoice", M.ThreadsOfFateString)
+
+-- Fix duplicate LFG applications after patch 9.1.5
+do
+	hooksecurefunc("LFGListSearchPanel_UpdateResultList", function(self)
+		if next(self.results) and next(self.applications) then
+			for _, value in ipairs(self.applications) do
+				tDeleteItem(self.results, value)
+			end
+			self.totalResults = #self.results
+
+			LFGListSearchPanel_UpdateResults(self)
+		end
+	end)
+end
