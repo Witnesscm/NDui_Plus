be6150e9e9796377bfc117fe7391a77489a55af0
Witnesscm <fanmicao@126.com>
2022-08-03 15:09:58 +0800

    Cleanup

--- Modules/Misc/Misc.lua
+++ Modules/Misc/Misc.lua
@@ -22,125 +22,137 @@ function M:OnLogin()
 	end
 end
 
--- DoubleClick to swap specialization.
-function M:DoubleClickSpecSwap()
-	for i = 1, GetNumSpecializations() do
-		local button = _G["PlayerTalentFrameSpecializationSpecButton"..i]
-		button:HookScript("OnDoubleClick", function() 
-			if i ~= GetSpecialization() then SetSpecialization(i) end
-		end)
+-- DoubleClick to swap specialization
+do
+	function M:DoubleClickSpecSwap()
+		for i = 1, GetNumSpecializations() do
+			local button = _G["PlayerTalentFrameSpecializationSpecButton"..i]
+			button:HookScript("OnDoubleClick", function() 
+				if i ~= GetSpecialization() then SetSpecialization(i) end
+			end)
 
-		button:HookScript("OnEnter", function() 
-			GameTooltip:AddLine(" ")
-			GameTooltip:AddLine(DB.LeftButton..L["QuickSpecSwap"], .6, .8, 1)
-			GameTooltip:Show()
-		end)
+			button:HookScript("OnEnter", function() 
+				GameTooltip:AddLine(" ")
+				GameTooltip:AddLine(DB.LeftButton..L["QuickSpecSwap"], .6, .8, 1)
+				GameTooltip:Show()
+			end)
+		end
 	end
+
+	P:AddCallbackForAddon("Blizzard_TalentUI", M.DoubleClickSpecSwap)
 end
-P:AddCallbackForAddon("Blizzard_TalentUI", M.DoubleClickSpecSwap)
 
 -- Auto collapse trade skill
-local function collapseAllCategories(refresh)
-	local list = _G.TradeSkillFrame.RecipeList
-	if not list or not list:IsShown() then return end
+do
+	local function collapseAllCategories(refresh)
+		local list = _G.TradeSkillFrame.RecipeList
+		if not list or not list:IsShown() then return end
 
-	if not refresh and not M.db["AutoCollapse"] then return end
+		if not refresh and not M.db["AutoCollapse"] then return end
 
-	list.tradeSkillChanged = nil
-	list.collapsedCategories = {}
+		list.tradeSkillChanged = nil
+		list.collapsedCategories = {}
 
-	if M.db["AutoCollapse"] then
-		for _, categoryID in ipairs({C_TradeSkillUI.GetCategories()}) do
-			list.collapsedCategories[categoryID] = true
-		end
-	end
-
-	list:Refresh()
-end
-
-function M:AutoCollapseTradeSkill()
-	local cb = CreateFrame("CheckButton", nil, _G.TradeSkillFrame, "OptionsCheckButtonTemplate")
-	cb:SetHitRectInsets(-5, -5, -5, -5)
-	cb:SetPoint("TOPRIGHT", -114, -2)
-	cb:SetSize(24, 24)
-	B.ReskinCheck(cb)
-	cb.text = B.CreateFS(cb, 14, L["AutoCollapse"], false, "LEFT", 25, 0)
-	cb:SetChecked(M.db["AutoCollapse"])
-	cb:SetScript("OnClick", function(self)
-		M.db["AutoCollapse"] = self:GetChecked()
-		collapseAllCategories(true)
-	end)
-
-	hooksecurefunc(_G.TradeSkillFrame.RecipeList, "OnDataSourceChanged", function()
-		collapseAllCategories()
-	end)
-end
-P:AddCallbackForAddon("Blizzard_TradeSkillUI", M.AutoCollapseTradeSkill)
-
--- Learn all available skills. Credit: TrainAll
-local function TrainAllButton_OnEnter(self)
-	GameTooltip:ClearLines()
-	GameTooltip:AddLine(format(L["Train All Cost"], self.Count, GetCoinTextureString(self.Cost)), 1, 1, 1)
-	GameTooltip:Show()
-end
-
-function M:TrainAllSkills()
-	local button = CreateFrame("Button", nil, ClassTrainerFrame, "MagicButtonTemplate")
-	button:SetPoint("RIGHT", ClassTrainerTrainButton, "LEFT", -2, 0)
-	button:SetText(L["Train All"])
-	button.Count = 0
-	button.Cost = 0
-	ClassTrainerFrame.TrainAllButton = button
-
-	button:SetScript("OnClick", function()
-		for i = 1, GetNumTrainerServices() do
-			if select(2, GetTrainerServiceInfo(i)) == "available" then
-				BuyTrainerService(i)
-			end
-		end
-	end)
-
-	button:SetScript("OnEnter", function(self)
-		GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-		self.UpdateTooltip = TrainAllButton_OnEnter
-		TrainAllButton_OnEnter(self)
-	end)
-
-	button:SetScript("OnLeave", function(self)
-		GameTooltip:Hide()
-		self.UpdateTooltip = nil
-	end)
-
-	if C.db["Skins"]["BlizzardSkins"] then
-		B.Reskin(button)
-	end
-
-	hooksecurefunc("ClassTrainerFrame_Update",function()
-		local sum, total = 0, 0;
-		for i = 1, GetNumTrainerServices() do
-			if select(2, GetTrainerServiceInfo(i)) == "available" then
-				sum = sum + 1
-				total = total + GetTrainerServiceCost(i)
+		if M.db["AutoCollapse"] then
+			for _, categoryID in ipairs({C_TradeSkillUI.GetCategories()}) do
+				list.collapsedCategories[categoryID] = true
 			end
 		end
 
-		button:SetEnabled(sum > 0)
-		button.Count = sum
-		button.Cost = total
-	end)
-end
-P:AddCallbackForAddon("Blizzard_TrainerUI", M.TrainAllSkills)
+		list:Refresh()
+	end
 
--- Autofill the Threads of Fate confirmation string
-function M:ThreadsOfFateString()
-	local fateDialog = StaticPopupDialogs["CONFIRM_PLAYER_CHOICE_WITH_CONFIRMATION_STRING"]
-	if fateDialog and fateDialog.OnShow then
-		hooksecurefunc(fateDialog, "OnShow", function(self)
-			self.editBox:SetText(SHADOWLANDS_EXPERIENCE_THREADS_OF_FATE_CONFIRMATION_STRING)
+	function M:AutoCollapseTradeSkill()
+		local cb = CreateFrame("CheckButton", nil, _G.TradeSkillFrame, "OptionsCheckButtonTemplate")
+		cb:SetHitRectInsets(-5, -5, -5, -5)
+		cb:SetPoint("TOPRIGHT", -114, -2)
+		cb:SetSize(24, 24)
+		B.ReskinCheck(cb)
+		cb.text = B.CreateFS(cb, 14, L["AutoCollapse"], false, "LEFT", 25, 0)
+		cb:SetChecked(M.db["AutoCollapse"])
+		cb:SetScript("OnClick", function(self)
+			M.db["AutoCollapse"] = self:GetChecked()
+			collapseAllCategories(true)
+		end)
+
+		hooksecurefunc(_G.TradeSkillFrame.RecipeList, "OnDataSourceChanged", function()
+			collapseAllCategories()
 		end)
 	end
+
+	P:AddCallbackForAddon("Blizzard_TradeSkillUI", M.AutoCollapseTradeSkill)
 end
-P:AddCallbackForAddon("Blizzard_PlayerChoice", M.ThreadsOfFateString)
+
+-- Learn all available skills. Credit: TrainAll
+do
+	local function TrainAllButton_OnEnter(self)
+		GameTooltip:ClearLines()
+		GameTooltip:AddLine(format(L["Train All Cost"], self.Count, GetCoinTextureString(self.Cost)), 1, 1, 1)
+		GameTooltip:Show()
+	end
+
+	function M:TrainAllSkills()
+		local button = CreateFrame("Button", nil, ClassTrainerFrame, "MagicButtonTemplate")
+		button:SetPoint("RIGHT", ClassTrainerTrainButton, "LEFT", -2, 0)
+		button:SetText(L["Train All"])
+		button.Count = 0
+		button.Cost = 0
+		ClassTrainerFrame.TrainAllButton = button
+
+		button:SetScript("OnClick", function()
+			for i = 1, GetNumTrainerServices() do
+				if select(2, GetTrainerServiceInfo(i)) == "available" then
+					BuyTrainerService(i)
+				end
+			end
+		end)
+
+		button:SetScript("OnEnter", function(self)
+			GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+			self.UpdateTooltip = TrainAllButton_OnEnter
+			TrainAllButton_OnEnter(self)
+		end)
+
+		button:SetScript("OnLeave", function(self)
+			GameTooltip:Hide()
+			self.UpdateTooltip = nil
+		end)
+
+		if C.db["Skins"]["BlizzardSkins"] then
+			B.Reskin(button)
+		end
+
+		hooksecurefunc("ClassTrainerFrame_Update",function()
+			local sum, total = 0, 0;
+			for i = 1, GetNumTrainerServices() do
+				if select(2, GetTrainerServiceInfo(i)) == "available" then
+					sum = sum + 1
+					total = total + GetTrainerServiceCost(i)
+				end
+			end
+
+			button:SetEnabled(sum > 0)
+			button.Count = sum
+			button.Cost = total
+		end)
+	end
+
+	P:AddCallbackForAddon("Blizzard_TrainerUI", M.TrainAllSkills)
+end
+
+-- Autofill the Threads of Fate confirmation string
+do
+	function M:ThreadsOfFateString()
+		local fateDialog = StaticPopupDialogs["CONFIRM_PLAYER_CHOICE_WITH_CONFIRMATION_STRING"]
+		if fateDialog and fateDialog.OnShow then
+			hooksecurefunc(fateDialog, "OnShow", function(self)
+				self.editBox:SetText(SHADOWLANDS_EXPERIENCE_THREADS_OF_FATE_CONFIRMATION_STRING)
+			end)
+		end
+	end
+
+	P:AddCallbackForAddon("Blizzard_PlayerChoice", M.ThreadsOfFateString)
+end
 
 -- Fix duplicate LFG applications after patch 9.1.5
 do
