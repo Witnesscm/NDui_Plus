bb0aa2893d7834a471854f8cbb39f6de2acfb265
Witnesscm <fanmicao@126.com>
2022-10-11 23:27:08 +0800

    ls_Toasts skin tweaks

--- Modules/Skins/ls_Toasts.lua
+++ Modules/Skins/ls_Toasts.lua
@@ -4,81 +4,91 @@ local S = P:GetModule("Skins")
 
 local _G = getfenv(0)
 
+local style = {
+	name = "NDui",
+	border = {
+		color = {0, 0, 0},
+		offset = 0,
+		size = 1,
+		texture = {1, 1, 1, 1},
+	},
+	title = {
+		flags = DB.Font[3],
+		shadow = false,
+	},
+	text = {
+		flags = DB.Font[3],
+		shadow = false,
+	},
+	icon = {
+		tex_coords = DB.TexCoord,
+	},
+	icon_border = {
+		color = {0, 0, 0},
+		offset = 0,
+		size = 1,
+		texture = {1, 1, 1, 1},
+	},
+	icon_text_1 = {
+		flags = DB.Font[3],
+	},
+	icon_text_2 = {
+		flags = DB.Font[3],
+	},
+	slot = {
+		tex_coords = DB.TexCoord,
+	},
+	slot_border = {
+		color = {0, 0, 0},
+		offset = 0,
+		size = 1,
+		texture = {1, 1, 1, 1},
+	},
+	glow = {
+		texture = {1, 1, 1, 1},
+		size = {226, 50},
+	},
+	shine = {
+		tex_coords = {403 / 512, 465 / 512, 15 / 256, 61 / 256},
+		size = {67, 50},
+		point = {
+			y = -1,
+		},
+	},
+	text_bg = {
+		hidden = true,
+	},
+	dragon = {
+		hidden = true,
+	},
+	icon_highlight = {
+		hidden = true,
+	},
+	bg = {
+		default = {
+			texture = "",
+		},
+	},
+}
+
+local function SkinToast(event, toast)
+	B.SetBD(toast)
+end
+
 function S:ls_Toasts()
 	if not S.db["ls_Toasts"] then return end
 
-	local LE, LC, LL = unpack(_G.ls_Toasts)
-	LE:RegisterSkin("ndui", {
-		name = "NDui",
-		border = {
-			color = {0, 0, 0},
-			offset = 0,
-			size = 1,
-			texture = {1, 1, 1, 1},
-		},
-		icon = {
-			tex_coords = DB.TexCoord,
-		},
-		icon_border = {
-			color = {0, 0, 0},
-			offset = 0,
-			size = 1,
-			texture = {1, 1, 1, 1},
-		},
-		slot = {
-			tex_coords = DB.TexCoord,
-		},
-		slot_border = {
-			color = {0, 0, 0},
-			offset = 0,
-			size = 1,
-			texture = {1, 1, 1, 1},
-		},
-		glow = {
-			texture = {1, 1, 1, 1},
-			size = {226, 50},
-		},
-		shine = {
-			tex_coords = {403 / 512, 465 / 512, 15 / 256, 61 / 256},
-			size = {67, 50},
-			point = {
-				y = -1,
-			},
-		},
-		text_bg = {
-			hidden = true,
-		},
-		dragon = {
-			hidden = true,
-		},
-		icon_highlight = {
-			hidden = true,
-		},
-		bg = {
-			default = {
-				texture = {0.06, 0.06, 0.06, 0.8},
-			},
-		},
-	})
-
+	local LE, LC  = unpack(_G.ls_Toasts)
+	LE:RegisterSkin("ndui", style)
+	LE:RegisterCallback("ToastCreated", SkinToast)
 	LC.db.profile.skin = "ndui"
 	LC.options.args.general.args.skin.disabled = true
+end
 
-	local function reskinFunc()
-		local index = 1
-		local toast = _G["LSToast"..index]
-		while toast do
-			if toast.BG and not toast.styled then
-				toast.BG:SetTexture(nil)
-				B.SetBD(toast)
-				toast.SetBackground = B.Dummy
-				toast.styled = true
-			end
-			index = index + 1
-			toast = _G["LSToast"..index]
-		end
-	end
-	hooksecurefunc(LE, "GetToast", reskinFunc)
+function S:LSPreviewBoxCurrency(widget)
+	S:Ace3_EditBox(widget)
+	P.ReskinTooltip(widget.preview)
 end
 
 S:RegisterSkin("ls_Toasts", S.ls_Toasts)
+S:RegisterAceGUIWidget("LSPreviewBoxCurrency")
