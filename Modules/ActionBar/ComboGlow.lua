3f4ddbdf7602db8605eb2aaffe2ee089f03ec738
Witnesscm <fanmicao@126.com>
2022-11-26 03:15:10 +0800

    Refactor finishing move highlight

--- Modules/ActionBar/ComboGlow.lua
+++ Modules/ActionBar/ComboGlow.lua
@@ -2,26 +2,30 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 local AB = P:GetModule("ActionBar")
 
+local LAB = LibStub("LibActionButton-1.0")
+local ActionButtons = LAB.actionButtons
+
+-- https://www.wowhead.com/cn/resource/4
 local AllGlowSpells = {
 	["ROGUE"] = {
-		[408] = true,
-		[1943] = true,
-		[2098] = true,
-		[32645] = true,
-		[121411] = true,
-		[196819] = true,
-		[269513] = true,
-		[280719] = true,
-		[315341] = true,
-		[315496] = true,
-		[319175] = true,
+		[408] = true,		-- 肾击
+		[1943] = true,		-- 割裂
+		[2098] = true,		-- 斩击
+		[32645] = true,		-- 毒伤
+		[121411] = true,	-- 猩红风暴
+		[196819] = true,	-- 刺骨
+		[269513] = true,	-- 天降杀机
+		[280719] = true,	-- 影分身
+		[315341] = true,	-- 正中眉心
+		[315496] = true,	-- 切割
+		[319175] = true,	-- 黑火药
 	},
 	["DRUID"] = {
-		[1079] = true,
-		[22568] = true,
-		[22570] = true,
-		[52610] = true,
-		[285381] = true,
+		[1079] = true,		-- 割裂
+		[22568] = true,		-- 凶猛撕咬
+		[22570] = true,		-- 割碎
+		[52610] = true,		-- 野蛮咆哮
+		[285381] = true,	-- 原始之怒
 	}
 }
 
@@ -36,47 +40,26 @@ function AB:UpdateMaxCombo()
 	AB.MaxComboPoints = UnitPowerMax("player", Enum.PowerType.ComboPoints)
 end
 
-function AB:UpdateOverlayGlow()
-	local spellType, id = GetActionInfo(self.action)
-	if spellType == "spell" and IsSpellOverlayed(id) then
+function AB:ComboGlow_Update()
+	local spellId = self:GetSpellId()
+	if spellId and IsSpellOverlayed(spellId) then
 		B.ShowOverlayGlow(self)
-	elseif spellType == "macro" then
-		local spellId = GetMacroSpell(id)
-		if spellId and IsSpellOverlayed(spellId) then
-			B.ShowOverlayGlow(self)
-		else
-			B.HideOverlayGlow(self)
-		end
 	else
 		B.HideOverlayGlow(self)
 	end
 end
 
-function AB:ActionBar_Update(event, ...)
-	local button = self:GetParent()
-	if event == "UNIT_POWER_UPDATE" then
-		local unit, powerType = ...
-		if unit == "player" and powerType == "COMBO_POINTS" then
-			AB.UpdateOverlayGlow(button)
+function AB:ComboGlow_OnEvent( ...)
+	local unit, powerType = ...
+	if unit == "player" and powerType == "COMBO_POINTS" then
+		for button in next, ActionButtons do
+			AB.ComboGlow_Update(button)
 		end
-	else
-		C_Timer.After(.1, function()
-			AB.UpdateOverlayGlow(button)
-		end)
 	end
 end
 
-function AB:CreateHandler()
-	local Bar = B:GetModule("Actionbar")
-	for _, button in ipairs(Bar.buttons) do
-		if button.action then
-			button.__handler = CreateFrame("Frame", nil, button)
-			button.__handler:RegisterEvent("UNIT_POWER_UPDATE")
-			button.__handler:RegisterEvent("ACTIONBAR_SLOT_CHANGED")
-			button.__handler:RegisterEvent("ACTIONBAR_PAGE_CHANGED")
-			button.__handler:SetScript("OnEvent", AB.ActionBar_Update)
-		end
-	end
+function AB:ComboGlow_OnButtonUpdate(button)
+	AB.ComboGlow_Update(button)
 end
 
 function AB:ComboGlow()
@@ -85,7 +68,8 @@ function AB:ComboGlow()
 	AB.GlowSpells = AllGlowSpells[DB.MyClass]
 	if not AB.GlowSpells then return end
 
-	AB:CreateHandler()
 	AB:UpdateMaxCombo()
 	B:RegisterEvent("UNIT_MAXPOWER", AB.UpdateMaxCombo)
+	B:RegisterEvent("UNIT_POWER_UPDATE", AB.ComboGlow_OnEvent)
+	LAB:RegisterCallback("OnButtonUpdate", AB.ComboGlow_OnButtonUpdate)
 end
