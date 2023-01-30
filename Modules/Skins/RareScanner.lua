fe7e0538ce0d3be2f570cc115bc85c2f1dec873c
Witnesscm <fanmicao@126.com>
2023-01-30 20:43:10 +0800

    Update RareScanner skin

--- Modules/Skins/RareScanner.lua
+++ Modules/Skins/RareScanner.lua
@@ -10,50 +10,76 @@ function S:RareScanner()
 	-- Scanner Button
 	local button = _G.scanner_button
 	if not button then return end
+
 	B.StripTextures(button)
 	B.Reskin(button)
 
 	local close = button.CloseButton
-	B.ReskinClose(close)
-	close:SetHitRectInsets(0, 0, 0, 0)
-	close:SetScale(1)
-	close:ClearAllPoints()
-	close:SetPoint("BOTTOMRIGHT",-5, 5)
+	if close then
+		B.ReskinClose(close)
+		close:SetHitRectInsets(0, 0, 0, 0)
+		close:SetScale(1)
+		close:ClearAllPoints()
+		close:SetPoint("BOTTOMRIGHT",-5, 5)
+	end
 
-	B.ReskinArrow(button.FilterDisabledButton, "up")
-	B.ReskinArrow(button.FilterEnabledButton, "down")
+	if button.FilterEntityButton and button.UnfilterEnabledButton then
+		B.ReskinArrow(button.FilterEntityButton, "down")
+		B.ReskinArrow(button.UnfilterEnabledButton, "up")
+	end
 
-	P.ReskinTooltip(button.LootBar.LootBarToolTipComp1)
-	P.ReskinTooltip(button.LootBar.LootBarToolTipComp2)
-
-	-- RSLoot
-	local itemFramesPool = button.LootBar and button.LootBar.itemFramesPool
-	if itemFramesPool and itemFramesPool.ShowIfReady then
-		hooksecurefunc(itemFramesPool, "ShowIfReady", function(self)
-			for itemFrame in self:EnumerateActive() do
-				if not itemFrame.styled then
-					B.ReskinIcon(itemFrame.Icon)
-					itemFrame.HL = itemFrame:CreateTexture(nil, "HIGHLIGHT")
-					itemFrame.HL:SetColorTexture(1, 1, 1, .25)
-					itemFrame.HL:SetAllPoints(itemFrame.Icon)
-
-					itemFrame.styled = true
-				end
+	-- LootBar
+	local LootBar = button.LootBar
+	if LootBar then
+		for _, key in ipairs({"LootBarToolTipComp1", "LootBarToolTipComp2"}) do
+			local tip = LootBar[key]
+			if tip then
+				P.ReskinTooltip(tip)
+				P.ReskinTooltip(tip)
 			end
-		end)
+		end
+
+		local itemFramesPool = button.LootBar.itemFramesPool
+		if itemFramesPool and itemFramesPool.ShowIfReady then
+			hooksecurefunc(itemFramesPool, "ShowIfReady", function(self)
+				for itemFrame in self:EnumerateActive() do
+					if not itemFrame.styled then
+						B.ReskinIcon(itemFrame.Icon)
+						itemFrame.HL = itemFrame:CreateTexture(nil, "HIGHLIGHT")
+						itemFrame.HL:SetColorTexture(1, 1, 1, .25)
+						itemFrame.HL:SetAllPoints(itemFrame.Icon)
+
+						itemFrame.styled = true
+					end
+				end
+			end)
+		end
 	end
 
 	-- RSSearch
-	for _, frame in ipairs(_G.WorldMapFrame.overlayFrames) do
-		local numChildren = frame:GetNumChildren()
-		if frame:GetObjectType() == "Frame" and numChildren == 1 and frame.EditBox then
-			frame:ClearAllPoints()
-			frame:SetPoint("TOP", _G.WorldMapFrame:GetCanvasContainer(), "TOP", 0, 0)
-			frame.EditBox:DisableDrawLayer("BACKGROUND")
-			B.ReskinInput(frame.EditBox, 20)
+	for _, child in pairs({_G.WorldMapFrame:GetChildren()}) do
+		if child:GetObjectType() == "Frame" and child.EditBox and child.RefreshPOIs then
+			child:ClearAllPoints()
+			child:SetPoint("TOP", _G.WorldMapFrame:GetCanvasContainer(), "TOP", 0, 0)
+			child.EditBox:DisableDrawLayer("BACKGROUND")
+			B.ReskinInput(child.EditBox, 20)
 			break
 		end
 	end
 end
 
+function S:LibQTip_RS()
+	local LibQTip = _G.LibStub and _G.LibStub("LibQTip-1.0RS", true)
+	if not LibQTip then return end
+
+	local origAcquire = LibQTip.Acquire
+	LibQTip.Acquire = function(...)
+		local tooltip = origAcquire(...)
+		P.ReskinTooltip(tooltip)
+
+		return tooltip
+	end
+end
+
 S:RegisterSkin("RareScanner", S.RareScanner)
+S:RegisterSkin("LibQTip_RS")
