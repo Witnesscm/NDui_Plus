88e948c84b31ba19b493fdeadc66030dad89f039
Witnesscm <fanmicao@126.com>
2023-05-05 20:18:47 +0800

    Update MDT skin

--- Modules/Skins/MythicDungeonTools.lua
+++ Modules/Skins/MythicDungeonTools.lua
@@ -2,52 +2,73 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 local S = P:GetModule("Skins")
 
+local Buttons = {
+	"sidePanelNewButton",
+	"sidePanelRenameButton",
+	"sidePanelImportButton",
+	"sidePanelExportButton",
+	"sidePanelDeleteButton",
+	"LinkToChatButton",
+	"ClearPresetButton",
+	"LiveSessionButton",
+	"MDIButton",
+}
+
 function S:MythicDungeonTools()
-	local MDT = _G.MDT or _G.DungeonTools
+	local MDT = _G.MDT
 	if not MDT then return end
 
-	local Buttons = {
-		"sidePanelNewButton",
-		"sidePanelRenameButton",
-		"sidePanelImportButton",
-		"sidePanelExportButton",
-		"sidePanelDeleteButton",
-		"LinkToChatButton",
-		"ClearPresetButton",
-		"LiveSessionButton",
-		"MDIButton",
-	}
-
 	local styled
-	hooksecurefunc(MDT, "ShowInterface", function()
-		if styled then return end
+	hooksecurefunc(MDT, "Async", function(_, _, name)
+		if name ~= "showInterface" or styled then return end
 
-		local frame = MDT.main_frame
-		frame.HelpButton.Ring:Hide()
+		P:Delay(1, function()
+			local frame = MDT.main_frame
+			if not frame then return end
 
-		local closeButton = frame.closeButton
-		B.ReskinClose(closeButton, frame.sidePanel, -7, -7)
-		closeButton:SetSize(20, 20)
+			frame.HelpButton.Ring:Hide()
 
-		local maximize = frame.maximizeButton
-		B.ReskinMinMax(maximize)
-		maximize.MaximizeButton:SetSize(20, 20)
-		maximize.MinimizeButton:SetSize(20, 20)
-
-		for _, v in pairs(Buttons) do
-			local button = frame[v] and frame[v].frame
-			if button and button.__bg and button.__gradient then
-				button:HookScript("OnEnter", P.Button_OnEnter)
-				button:HookScript("OnLeave", P.Button_OnLeave)
+			local closeButton = frame.closeButton
+			if closeButton then
+				B.ReskinClose(closeButton, frame.sidePanel, -7, -7)
+				closeButton:SetSize(20, 20)
 			end
-		end
 
-		local progressBar = frame.sidePanel.ProgressBar.Bar
-		B.StripTextures(progressBar)
-		progressBar:SetStatusBarTexture(DB.normTex)
-		B.CreateBDFrame(progressBar, .25)
+			local maximize = frame.maximizeButton
+			if maximize then
+				B.ReskinMinMax(maximize)
+				maximize.MaximizeButton:SetSize(20, 20)
+				maximize.MinimizeButton:SetSize(20, 20)
+			end
 
-		styled = true
+			for _, key in pairs(Buttons) do
+				local button = frame[key] and frame[key].frame
+				if button and button.__bg and button.__gradient then
+					button:HookScript("OnEnter", P.Button_OnEnter)
+					button:HookScript("OnLeave", P.Button_OnLeave)
+				end
+			end
+
+			local progressBar = frame.sidePanel and frame.sidePanel.ProgressBar and frame.sidePanel.ProgressBar.Bar
+			if progressBar then
+				B.StripTextures(progressBar)
+				progressBar:SetStatusBarTexture(DB.normTex)
+				B.CreateBDFrame(progressBar, .25)
+			end
+
+			for _, key in ipairs({"topPanelTex", "bottomPanelTex", "sidePanelTex"}) do
+				frame[key]:SetAlpha(0)
+			end
+
+			local bg = B.SetBD(frame)
+			bg:SetPoint("TOPLEFT", frame.topPanel)
+			bg:SetPoint("BOTTOMRIGHT", frame.sidePanel)
+
+			P.ReskinTooltip(MDT.tooltip)
+			P.ReskinTooltip(MDT.pullTooltip)
+
+			styled = true
+		end)
 	end)
 
 	local enemyStyled
@@ -77,4 +98,3 @@ function S:MythicDungeonTools()
 end
 
 S:RegisterSkin("MythicDungeonTools", S.MythicDungeonTools)
-S:RegisterSkin("DungeonTools", S.MythicDungeonTools)
