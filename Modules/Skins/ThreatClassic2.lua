b0dffaee824c5cc27fad573ee56905a0401dce4d
Witnesscm <fanmicao@126.com>
2023-12-16 06:03:24 +0800

    Update ThreatClassic2 skin

--- Modules/Skins/ThreatClassic2.lua
+++ Modules/Skins/ThreatClassic2.lua
@@ -4,14 +4,25 @@ local S = P:GetModule("Skins")
 
 local _G = getfenv(0)
 
-local function reskinStatusBar(self)
+local function DisableBackdrop(self)
+	if not self then return end
+
 	self:SetBackdrop(nil)
 	self.SetBackdrop = B.Dummy
+	DisableBackdrop(self.edgeBackdrop)
+end
 
-	local backdrop = self.edgeBackdrop or self.backdrop
-	if backdrop then
-		backdrop:SetBackdrop(nil)
-		backdrop.SetBackdrop = B.Dummy
+local function resetAnchor(self, point)
+	local bg = self.parent.__bg
+	bg:ClearAllPoints()
+	if not point then
+		bg:SetOutside()
+	elseif point == "TOPLEFT" then
+		bg:SetPoint("TOPLEFT", -C.mult, C.mult)
+		bg:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", C.mult, -C.mult)
+	elseif point == "BOTTOMLEFT" then
+		bg:SetPoint("TOPLEFT", self, "TOPLEFT", -C.mult, C.mult)
+		bg:SetPoint("BOTTOMRIGHT", C.mult, -C.mult)
 	end
 end
 
@@ -22,57 +33,39 @@ function S:ThreatClassic2()
 		local frame = _G.ThreatClassic2BarFrame
 		if not frame then return end
 
-		local bg = B.SetBD(frame)
-		if frame.header:IsShown() then
-			bg:SetPoint("TOPLEFT", -C.mult, 18)
-		end
+		B.StripTextures(frame)
+		frame.__bg = B.SetBD(frame)
 
-		local frameBg = frame.bg
-		if frameBg then
-			frameBg:SetColorTexture(0, 0, 0, 0)
-			frameBg:SetVertexColor(0, 0, 0, 0)
-			frameBg.SetVertexColor = B.Dummy
+		if frame.bg then
+			frame.bg.SetVertexColor = B.Dummy
 		end
 
 		local header = frame.header
 		if header then
-			reskinStatusBar(header)
+			DisableBackdrop(header)
 			header:SetStatusBarColor(0, 0, 0, 0)
 			header.SetStatusBarColor = B.Dummy
-			header.text:SetPoint("LEFT", header, 4, 0)
+			header.SetStatusBarTexture = B.Dummy
+			header.text:SetPoint("LEFT", header, 4, 1)
+			header.parent = frame
+			hooksecurefunc(header, "SetPoint", resetAnchor)
+			hooksecurefunc(header, "Hide", resetAnchor)
 		end
 
 		for _, child in pairs {frame:GetChildren()} do
-			if child:GetObjectType() == "StatusBar" and child.bg and child.val then
-				reskinStatusBar(child)
-				child.bg:SetVertexColor(0, 0, 0, 0)
-				child.bg.SetVertexColor = B.Dummy
+			if child:GetObjectType() == "StatusBar" and child.val then
+				DisableBackdrop(child)
 			end
+		end
+
+		-- Lock the options of bar texture and padding
+		local options = LibStub("AceConfigRegistry-3.0"):GetOptionsTable("ThreatClassic2", "cmd", "MyLib-1.0")
+		if options then
+			local set = options.args.appearance.set
+			set({nil, "bar", "texture"}, "normTex")
+			set({nil, "bar", "padding"}, 2)
 		end
 	end)
 end
 
 S:RegisterSkin("ThreatClassic2", S.ThreatClassic2)
-
-local function loadStyle(event, addon)
-	if addon ~= "ThreatClassic2" then return end
-
-	local charKey = DB.MyName .. " - " .. DB.MyRealm
-	ThreatClassic2DB = ThreatClassic2DB or {}
-	ThreatClassic2DB["profileKeys"] = ThreatClassic2DB["profileKeys"] or {}
-	ThreatClassic2DB["profileKeys"][charKey] = ThreatClassic2DB["profileKeys"][charKey] or charKey
-	ThreatClassic2DB["profiles"] = ThreatClassic2DB["profiles"] or {}
-	ThreatClassic2DB["profiles"][charKey] = ThreatClassic2DB["profiles"][charKey] or {}
-
-	local profileKey = ThreatClassic2DB["profileKeys"][charKey]
-	local profile = profileKey and ThreatClassic2DB["profiles"][profileKey]
-	if profile then
-		profile.bar = profile.bar or {}
-		profile.bar.texture = profile.bar.texture or "normTex"
-		profile.bar.padding = 2
-	end
-
-	B:UnregisterEvent(event, loadStyle)
-end
-
-B:RegisterEvent("ADDON_LOADED", loadStyle)
