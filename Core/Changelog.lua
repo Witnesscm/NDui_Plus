7814ee0a89104f677560ca89523063e64047c7d3
Witnesscm <fanmicao@126.com>
2024-02-20 23:28:30 +0800

    3.4.1

--- Core/Changelog.lua
+++ Core/Changelog.lua
@@ -2,6 +2,10 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 
 P.Changelog = [[
+v3.4.1
+[动作条] 添加终结技高亮
+[美化] 更新Auctionator美化
+
 v3.4.0
 [易用性] 添加缺失的雕文法术图标, 调整雕文插槽闪光逻辑
 [美化] 修复Krowi_AchievementFilter美化
