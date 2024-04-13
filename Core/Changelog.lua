de3343ee479728040fed9aed2dd4ef7c67a1ee21
Witnesscm <fanmicao@126.com>
2024-04-13 22:36:48 +0800

    10.2.2

--- Core/Changelog.lua
+++ Core/Changelog.lua
@@ -2,6 +2,12 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 
 P.Changelog = [[
+v10.2.2
+[主要] 更新版本号至10.2.6
+[聊天] 修复启用聊天框隐藏时背景层级问题
+[易用性] 在PTR中暂时禁用兽栏面板扩展功能
+[美化] 添加BetterBags 美化
+
 v10.2.1
 [主要] 更新版本号至10.2.5; 适配部分API改动
 [美化] 修复Auctionator美化
