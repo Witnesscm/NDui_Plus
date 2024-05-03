364c09f8a314a2a8bf77e330c7bee71c859bca76
Witnesscm <fanmicao@126.com>
2024-05-03 08:18:31 +0800

    10.2.3

--- Core/Changelog.lua
+++ Core/Changelog.lua
@@ -2,11 +2,16 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 
 P.Changelog = [[
+v10.2.3
+[鼠标提示] PvE进度信息更新巨龙时代S4数据
+[美化] 添加BuyEmAll美化
+[美化] 修复TalentLoadoutsEx美化
+
 v10.2.2
 [主要] 更新版本号至10.2.6
 [聊天] 修复启用聊天框隐藏时背景层级问题
 [易用性] 在PTR中暂时禁用兽栏面板扩展功能
-[美化] 添加BetterBags 美化
+[美化] 添加BetterBags美化
 
 v10.2.1
 [主要] 更新版本号至10.2.5; 适配部分API改动
