185542dc2f8cc67a2cf25bfcd1246e4a1a06ba03
Witnesscm <fanmicao@126.com>
2024-05-26 15:41:57 +0800

    feat(skins): add ReforgeLite skin

---
+++ Modules/Skins/ReforgeLite.lua
@@ -0,0 +1,101 @@
+local _, ns = ...
+local B, C, L, DB, P = unpack(ns)
+local S = P:GetModule("Skins")
+
+local function reskinCollapse(self)
+	if not self.button then return end
+
+	P.ReskinCollapse(self.button)
+	self.button.SetPushedTexture = B.Dummy
+	self.button:UpdateTexture()
+end
+
+local function SkinRLWidget(self)
+	if not self or self.styled then return end
+
+	local objType = self and self:GetObjectType()
+	if objType == "EditBox" then
+		B.ReskinInput(self, 20)
+	elseif objType == "Frame" and self.Button then
+		P.ReskinDropDown(self)
+		self.Button:SetPoint("RIGHT", -18, 8)
+	elseif objType == "CheckButton" then
+		B.ReskinCheck(self)
+	end
+
+	self.styled = true
+end
+
+local function SkinNewRLWidget(self)
+	SkinRLWidget(_G["ReforgeLiteWidget"..self.widgetCount])
+end
+
+local function SkinMethodCategory(self)
+	if self.methodCategory and not self.methodCategory.styled then
+		reskinCollapse(self.methodCategory)
+		S:Proxy("Reskin", self.methodShow)
+		S:Proxy("Reskin", self.methodReset)
+
+		self.methodCategory.styled = true
+	end
+end
+
+function S:ReforgeLite()
+	local frame = _G.ReforgeLite
+	if not frame then return end
+
+	B.StripTextures(frame)
+	B.SetBD(frame)
+	S:Proxy("ReskinClose", frame.close)
+	S:Proxy("ReskinScroll", frame.scrollBar)
+	S:Proxy("ReskinArrow", frame.presetsButton, "down")
+	S:Proxy("ReskinSlider", frame.quality)
+
+	for _, key in ipairs({"savePresetButton", "deletePresetButton", "pawnButton", "computeButton", "storedClear", "storedRestore", "debugButton"}) do
+		S:Proxy("Reskin", frame[key])
+	end
+
+	for _, key in ipairs({"statWeightsCategory", "storedCategory", "settingsCategory"}) do
+		reskinCollapse(frame[key])
+	end
+
+	for _, cap in ipairs(frame.statCaps) do
+		S:Proxy("Reskin", cap.add)
+		cap.add.text = B.CreateFS(cap.add, 18, "+", false, "CENTER", 2, 0)
+	end
+
+	SkinMethodCategory(frame)
+	hooksecurefunc(frame, "UpdateMethodCategory", SkinMethodCategory)
+
+	hooksecurefunc(frame, "ShowMethodWindow", function()
+		if frame.methodWindow and not frame.methodWindow.styled then
+			B.StripTextures(frame.methodWindow)
+			B.SetBD(frame.methodWindow)
+			S:Proxy("ReskinClose", frame.methodWindow.close)
+			S:Proxy("Reskin", frame.methodWindow.reforge)
+
+			frame.methodWindow.styled = true
+		end
+	end)
+
+	-- Widget
+	local GUI = _G.ReforgeLiteGUI
+	for index = 1, GUI.widgetCount do
+		SkinRLWidget(_G["ReforgeLiteWidget"..index])
+	end
+
+	hooksecurefunc(GUI, "CreateEditBox", SkinNewRLWidget)
+	hooksecurefunc(GUI, "CreateDropdown", SkinNewRLWidget)
+	hooksecurefunc(GUI, "CreateCheckButton", SkinNewRLWidget)
+
+	-- ErrorFrame
+	local ErrorFrame = _G.ReforgeLiteErrorFrame
+	if ErrorFrame then
+		B.StripTextures(ErrorFrame)
+		B.SetBD(ErrorFrame)
+		S:Proxy("Reskin", ErrorFrame.ok)
+		S:Proxy("ReskinScroll", ErrorFrame.scroll.ScrollBar)
+	end
+end
+
+S:RegisterSkin("ReforgeLite", S.ReforgeLite)
