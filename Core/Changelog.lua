dadedd20d60594cf5464111ac250ae59c61328c8
Witnesscm <fanmicao@126.com>
2024-05-26 17:45:58 +0800

    4.4.1

--- Core/Changelog.lua
+++ Core/Changelog.lua
@@ -2,6 +2,16 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 
 P.Changelog = [[
+v4.4.1
+[动作条] 更新终结技高亮法术列表
+[易用性] 添加兽栏面板扩展
+[易用性] 移除公会面板扩展
+[美化] 添加 ReforgeLit 美化
+[美化] 修复 Krowi_AchievementFilter 美化
+[美化] 修复 AutoBarCata 美化
+[易用性] 修复暴雪雕文面板鼠标滚动
+[易用性] 修复暴雪角色面板自动扩展
+
 v4.4.0
 [主要] 更新版本号至4.4.0
 [动作条] 移除法师动作条
