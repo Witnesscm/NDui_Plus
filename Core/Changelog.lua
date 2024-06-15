c0ed2dbfe39425d7f83aab7af1dabd160ef4ccad
Witnesscm <fanmicao@126.com>
2024-06-15 18:09:24 +0800

    3.4.2

--- Core/Changelog.lua
+++ Core/Changelog.lua
@@ -2,6 +2,11 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 
 P.Changelog = [[
+v3.4.2
+[聊天] 修复启用聊天框隐藏时背景层级问题
+[易用性] 移除天赋面板扩展职责选择
+[美化] 修复Krowi_AchievementFilter美化
+
 v3.4.1
 [动作条] 添加终结技高亮
 [美化] 更新Auctionator美化
