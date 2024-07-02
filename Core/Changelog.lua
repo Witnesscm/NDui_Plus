f10d066fdc39ab9ddc6dc810c3d5523c65833bc7
Witnesscm <fanmicao@126.com>
2024-07-02 18:02:14 +0800

    4.4.2

--- Core/Changelog.lua
+++ Core/Changelog.lua
@@ -2,6 +2,13 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 
 P.Changelog = [[
+v4.4.2
+[聊天] 修复启用聊天框隐藏时背景层级问题
+[美化] 修复 ReforgeLit 美化
+[美化] 修复 Restocker 美化
+[美化] 修复 ThreatClassic2 美化
+[易用性] 移除雕文面板鼠标滚动修复
+
 v4.4.1
 [动作条] 更新终结技高亮法术列表
 [易用性] 添加兽栏面板扩展
@@ -19,13 +26,4 @@ v4.4.0
 [易用性] 移除天赋面板扩展, 默认UI支持双击切换天赋
 [易用性] Roll点增强支持"分解"选项，热修后生效
 [美化] 更新MerInspect美化
-
-v3.4.1
-[动作条] 添加终结技高亮
-[美化] 更新Auctionator美化
-
-v3.4.0
-[易用性] 添加缺失的雕文法术图标, 调整雕文插槽闪光逻辑
-[美化] 修复Krowi_AchievementFilter美化
-[美化] 更新WeakAurasOptions美化
 ]]
