cf21936c9838e141ff9b02ab6dd5be3bc349b70a
Witnesscm <fanmicao@126.com>
2024-07-27 20:17:12 +0800

    11.0.1

--- Core/Changelog.lua
+++ Core/Changelog.lua
@@ -2,6 +2,14 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 
 P.Changelog = [[
+v11.0.1
+[主要] 适配11.0.2 API改动
+[美化] 添加 WeakAuras 美化提醒
+[美化] 添加 ShadowDancer 美化
+[美化] 调整 Immersion 按钮美化
+[鼠标提示] 修复PvE进度未正常对满级角色生效的问题
+[单位框体] 更新 oUF_Fader
+
 v11.0.0
 [主要] 更新版本号至11.0.0; 适配部分API改动
 [易用性] 调整启用商人面板扩展时售卖垃圾按钮位置
