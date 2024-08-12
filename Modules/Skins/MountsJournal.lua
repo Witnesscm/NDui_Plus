aefc6d9b1fd1754337e9f9a506fa541945dd2d55
Witnesscm <fanmicao@126.com>
2024-08-12 10:38:18 +0800

    feat(skins): update MountsJournal skin

--- Modules/Skins/MountsJournal.lua
+++ Modules/Skins/MountsJournal.lua
@@ -2,7 +2,7 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 local S = P:GetModule("Skins")
 
-local function reskinSummonButton(button)
+local function handleSummonButton(button)
 	if not button then
 		P.Developer_ThrowError("button is nil")
 		return
@@ -15,7 +15,7 @@ local function reskinSummonButton(button)
 	B.ReskinIcon(button.icon, true)
 end
 
-local function reskinDropDownMenu(_, level)
+local function toggleDropDownMenu(_, level)
 	local LibSFDropDown = LibStub("LibSFDropDown-1.5", true)
 	if not LibSFDropDown then return end
 
@@ -32,10 +32,23 @@ end
 
 local function handledDropDown(button)
 	if not button or not button.ddToggle then
+		P.Developer_ThrowError("dropdown menu is nil")
 		return
 	end
 
-	hooksecurefunc(button, "ddToggle", reskinDropDownMenu)
+	hooksecurefunc(button, "ddToggle", toggleDropDownMenu)
+end
+
+local function handledFilterButton(button)
+	if not button then
+		P.Developer_ThrowError("filter button is nil")
+		return
+	end
+
+	B.ReskinFilterButton(button)
+	button.__bg:SetPoint("TOPLEFT", 1, 0)
+	button.__bg:SetPoint("BOTTOMRIGHT", -1, 0)
+	handledDropDown(button)
 end
 
 local function updateVisibility(show)
@@ -47,23 +60,140 @@ local function updateVisibility(show)
 	end
 end
 
+local function handlePetIcon(self)
+	self.bg = B.ReskinIcon(self.icon)
+	B.ReskinIconBorder(self.qualityBorder)
+	self.levelBG:SetAlpha(0)
+end
+
+local function handlePetButton(self)
+	if not self.styled then
+		self.background:SetAlpha(0)
+		self.bg = B.CreateBDFrame(self.background, .25)
+		self.bg:SetInside(self.background)
+		local hl = self:GetHighlightTexture()
+		hl:SetColorTexture(1, 1, 1, .25)
+		hl:SetInside(self.bg)
+		self.selectedTexture:SetColorTexture(DB.r, DB.g, DB.b, .25)
+		self.selectedTexture:SetInside(self.bg)
+		handlePetIcon(self.infoFrame)
+
+		self.styled = true
+	end
+end
+
+local function handlePetList(self)
+	local list = self.petSelectionList
+	if list and not list.styled then
+		B.StripTextures(list)
+		B.SetBD(list)
+		S:Proxy("ReskinClose", list.closeButton)
+		S:Proxy("StripTextures", list.controlPanel)
+		S:Proxy("ReskinInput", list.searchBox)
+		S:Proxy("StripTextures", list.filtersPanel)
+		S:Proxy("StripTextures", list.controlButtons)
+		S:Proxy("StripTextures", list.petListFrame)
+		handlePetButton(list.randomFavoritePet)
+		handlePetButton(list.randomPet)
+		handlePetButton(list.noPet)
+
+		local buttons = list.filtersPanel and list.filtersPanel.buttons
+		if buttons then
+			for _, button in pairs(buttons) do
+				button:SetBackdrop(nil)
+				button.bg = B.CreateBDFrame(button)
+				button.bg:SetInside()
+				button.icon:SetInside(button.bg)
+				button.icon:SetTexCoord(unpack(DB.TexCoord))
+				button.highlight:SetColorTexture(1, 1, 1, .25)
+				button.highlight:SetInside(button.bg)
+				button:SetCheckedTexture(DB.pushedTex)
+			end
+		end
+
+		local petListFrame = list.petListFrame
+		if petListFrame then
+			S:Proxy("ReskinTrimScroll", petListFrame.scrollBar)
+			hooksecurefunc(petListFrame.scrollBox, "Update", function(self)
+				self:ForEachFrame(handlePetButton)
+			end)
+		end
+
+		list.styled = true
+	end
+end
+
+local function handleMountButton(self)
+	if not self.bg then
+		self.bg = B.CreateBDFrame(self, .25)
+		self.bg:SetPoint("TOPLEFT", 4, -2)
+		self.bg:SetPoint("BOTTOMRIGHT", 0, 2)
+		self.background:SetAlpha(0)
+		self.selectedTexture:SetAlpha(0)
+		self:SetHighlightTexture(0)
+		self.name:ClearAllPoints()
+		self.name:SetPoint("LEFT", 10, 0)
+
+		local dragButton = self.dragButton
+		dragButton.icon:SetSize(dragButton:GetSize())
+		dragButton.bg = B.ReskinIcon(dragButton.icon)
+		B.ReskinIconBorder(dragButton.qualityBorder, true)
+		dragButton.activeTexture:SetAlpha(0)
+		local hl = dragButton.highlight
+		hl:SetColorTexture(1, 1, 1, .25)
+		hl:SetAllPoints(dragButton.bg)
+	end
+
+	if self.selectedTexture:IsShown() then
+		self.bg:SetBackdropColor(DB.r, DB.g, DB.b, .25)
+	else
+		self.bg:SetBackdropColor(0, 0, 0, .25)
+	end
+
+	if self.dragButton.activeTexture:IsShown() then
+		self.dragButton.bg:SetBackdropBorderColor(1, .8, 0)
+	end
+end
+
+local function handleGridMountButton(self)
+	for _, bu in ipairs(self.mounts) do
+		if not bu.bg then
+			bu.bg = B.ReskinIcon(bu.icon)
+			B.ReskinIconBorder(bu.qualityBorder, true)
+			bu.selectedTexture:SetColorTexture(1, .8, 0, .4)
+			local hl = bu.highlight
+			hl:SetColorTexture(1, 1, 1, .25)
+			hl:SetInside(bu.bg)
+		end
+	end
+end
+
 function S:MountsJournal()
 	if not C.db["Skins"]["BlizzardSkins"] then return end
 
 	local MountsJournal = _G.MountsJournal
 	if not MountsJournal then return end
 
+	P.ReskinTooltip(_G.MJTooltipModel)
+
 	local MountsJournalFrame = _G.MountsJournalFrame
+	hooksecurefunc(MountsJournalFrame, "ADDON_LOADED", function(self, addonName)
+		if addonName == "Blizzard_Collections" then
+			S:Proxy("ReskinCheck", self.useMountsJournalButton)
+		end
+	end)
+
 	hooksecurefunc(MountsJournalFrame, "init", function(self)
-		--MountsJournalBackground
 		local bgFrame = self.bgFrame
 		if bgFrame then
 			B.ReskinPortraitFrame(bgFrame)
 			S:Proxy("ReskinClose", bgFrame.closeButton)
 			S:Proxy("StripTextures", bgFrame.leftInset)
 			S:Proxy("StripTextures", bgFrame.rightInset)
+			S:Proxy("ReskinTrimScroll", bgFrame.leftInset and bgFrame.leftInset.scrollBar)
+			handledFilterButton(bgFrame.profilesMenu)
 
-			for _, key in ipairs({"btnConfig", "mountSpecial", "summonButton"}) do
+			for _, key in ipairs({"mountSpecial", "summonButton"}) do
 				S:Proxy("Reskin", bgFrame[key])
 			end
 
@@ -81,13 +211,11 @@ function S:MountsJournal()
 			end
 
 			for _, key in ipairs({"summon1", "summon2"}) do
-				reskinSummonButton(bgFrame[key])
+				handleSummonButton(bgFrame[key])
 			end
 
-			local profilesMenu = bgFrame.profilesMenu
-			if profilesMenu then
-				B.ReskinFilterButton(profilesMenu)
-				handledDropDown(profilesMenu)
+			for _, tab in ipairs(bgFrame.Tabs) do
+				B.ReskinTab(tab)
 			end
 
 			hooksecurefunc(bgFrame, "StopMovingOrSizing", function()
@@ -107,7 +235,8 @@ function S:MountsJournal()
 		S:Proxy("StripTextures", self.mountCount)
 		S:Proxy("CreateBDFrame", self.mountCount, .25)
 		S:Proxy("ReskinNavBar", self.navBar)
-		S:Proxy("ReskinCheck", self.useMountsJournalButton)
+		handledFilterButton(self.filtersButton)
+		handledDropDown(self.tags and self.tags.mountOptionsMenu)
 
 		local worldMap = self.worldMap
 		if worldMap then
@@ -121,12 +250,29 @@ function S:MountsJournal()
 			B.StripTextures(mapSettings)
 			S:Proxy("StripTextures", mapSettings.mapControl)
 			S:Proxy("Reskin", mapSettings.CurrentMap)
+			S:Proxy("Reskin", mapSettings.existingListsToggle)
+			S:Proxy("ReskinCheck", mapSettings.Flags)
+			handledFilterButton(mapSettings.listFromMap)
+			S:Proxy("StripTextures", mapSettings.relationMap)
+			S:Proxy("CreateBDFrame", mapSettings.relationMap, 0)
+			handledFilterButton(mapSettings.dnr)
+		end
 
-			local ndr = mapSettings.dnr
-			if ndr then
-				B.ReskinFilterButton(ndr)
-				handledDropDown(ndr)
-			end
+		local existingLists = self.existingLists
+		if existingLists then
+			B.StripTextures(existingLists)
+			existingLists.bg = B.SetBD(existingLists)
+			existingLists.bg:SetAllPoints()
+			S:Proxy("ReskinInput", existingLists.searchBox)
+			S:Proxy("ReskinTrimScroll", existingLists.scrollBar)
+
+			hooksecurefunc(existingLists, "toggleInit", function(_, btn)
+				if not btn.bg then
+					btn.bg = B.CreateBDFrame(btn, .25, true)
+					btn.bg:SetAllPoints(btn.toggle)
+				end
+				btn.toggle:SetAtlas(btn.category.expanded and "Soulbinds_Collection_CategoryHeader_Expand" or "Soulbinds_Collection_CategoryHeader_Collapse")
+			end)
 		end
 
 		local mountDisplay = self.mountDisplay
@@ -140,12 +286,25 @@ function S:MountsJournal()
 
 			local info = mountDisplay.info
 			if info then
-				B.ReskinIcon(info.icon)
+				S:Proxy("ReskinIcon", info.icon)
+				handledDropDown(info.linkLang)
+				handledDropDown(info.modelSceneSettingsButton)
+
+				local mountDescriptionToggle = info.mountDescriptionToggle
+				if mountDescriptionToggle then
+					B.Reskin(mountDescriptionToggle)
+					mountDescriptionToggle.__bg:SetPoint("TOPLEFT", 2, 0)
+					mountDescriptionToggle.__bg:SetPoint("BOTTOMRIGHT", -2, 0)
+				end
 
 				local petSelectionBtn = info.petSelectionBtn
 				if petSelectionBtn then
 					B.StripTextures(petSelectionBtn)
-					B.CreateBDFrame(petSelectionBtn, .25)
+					local bg = B.CreateBDFrame(petSelectionBtn, .25)
+					petSelectionBtn.highlight:SetColorTexture(1, 1, 1, .25)
+					petSelectionBtn.highlight:SetInside(bg)
+					handlePetIcon(petSelectionBtn.infoFrame)
+					petSelectionBtn:HookScript("OnClick", handlePetList)
 				end
 			end
 		end
@@ -153,16 +312,97 @@ function S:MountsJournal()
 		local filtersPanel = self.filtersPanel
 		if filtersPanel then
 			B.StripTextures(filtersPanel)
+			S:Proxy("ReskinInput", filtersPanel.searchBox)
+			S:Proxy("StripTextures", filtersPanel.shownPanel)
+
+			for _, key in ipairs({"btnToggle", "gridToggleButton"}) do
+				local bu = filtersPanel[key]
+				if bu then
+					B.Reskin(bu)
+					bu.__bg:SetInside()
+				end
+			end
 		end
+
+		local filtersBar = self.filtersBar
+		if filtersBar then
+			B.StripTextures(filtersBar)
+			for _, tab in ipairs(filtersBar.tabs) do
+				B.StripTextures(tab)
+				local bg = B.CreateBDFrame(tab, .25)
+				bg:SetPoint("TOPLEFT", 8, -3)
+				bg:SetPoint("BOTTOMRIGHT", -8, 0)
+				tab:SetHighlightTexture(DB.bdTex)
+				local HL = tab:GetHighlightTexture()
+				HL:SetColorTexture(1, 1, 1, .25)
+				HL:SetInside(bg)
+				B.StripTextures(tab.selected)
+				local selectedBG = tab.selected:CreateTexture(nil, "BACKGROUND")
+				selectedBG:SetInside(bg)
+				selectedBG:SetColorTexture(DB.r, DB.g, DB.b, .25)
+
+				for _, bu in ipairs(tab.content.childs) do
+					local isSquare = B:Round(bu:GetHeight()) == B:Round(bu:GetWidth())
+					bu:SetBackdrop(nil)
+					bu.bg = B.CreateBDFrame(bu, .25)
+					bu.bg:SetInside()
+					local hl = bu:GetHighlightTexture()
+					hl:SetColorTexture(1, 1, 1, .25)
+					hl:SetInside(bu.bg)
+					if isSquare then
+						bu:SetCheckedTexture(DB.pushedTex)
+					else
+						local check = bu:GetCheckedTexture()
+						check:SetColorTexture(DB.r, DB.g, DB.b, .25)
+						check:SetInside(bu.bg)
+					end
+				end
+			end
+		end
+
+		local modelScene = self.modelScene
+		if modelScene then
+			local animationsCombobox = modelScene.animationsCombobox
+			if animationsCombobox then
+				P.ReskinDropDown(animationsCombobox)
+				animationsCombobox.Button:SetPoint("RIGHT", -6, 0)
+				handledDropDown(animationsCombobox)
+			end
+
+			local modelControl = modelScene.modelControl
+			if modelControl then
+				B.StripTextures(modelControl)
+				for _, bu in pairs({modelControl:GetChildren()}) do
+					bu.bg:SetAlpha(0)
+				end
+			end
+		end
+	end)
+
+	hooksecurefunc(MountsJournalFrame, "defaultInitMountButton", function(self, button)
+		handleMountButton(button)
+	end)
+
+	hooksecurefunc(MountsJournalFrame, "grid3InitMountButton", function(self, button)
+		handleGridMountButton(button)
 	end)
 
 	local summonPanel = MountsJournal.summonPanel
 	if summonPanel then
 		B.StripTextures(summonPanel)
 		B.SetBD(summonPanel)
-		reskinSummonButton(summonPanel.summon1)
-		reskinSummonButton(summonPanel.summon2)
+		handleSummonButton(summonPanel.summon1)
+		handleSummonButton(summonPanel.summon2)
 		handledDropDown(summonPanel.contextMenu)
 	end
+
+	local util = _G.MountsJournalUtil
+	local origcreateCheckbox = util.createCheckboxChild
+	util.createCheckboxChild = function(...)
+		local check = origcreateCheckbox(...)
+		B.ReskinCheck(check)
+		return check
+	end
 end
+
 S:RegisterSkin("MountsJournal", S.MountsJournal)
