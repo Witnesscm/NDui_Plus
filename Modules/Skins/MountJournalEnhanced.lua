88ce929a8cf50bf84b685618a4d195e624931038
Witnesscm <fanmicao@126.com>
2025-01-21 21:02:24 +0800

    fix: fix Mount Journal Enhanced skin

--- Modules/Skins/MountJournalEnhanced.lua
+++ Modules/Skins/MountJournalEnhanced.lua
@@ -22,68 +22,67 @@ end
 
 local styled
 local function ReskinMJEnhanced()
-	if styled then return end
-
-	P.WaitFor(function()
-		local callbacks = EventRegistry:GetCallbacksByEvent(2, "MountJournal.OnShow")
-		return not callbacks["MountJournalEnhanced"]
-	end, function()
-		for _, child in pairs({_G.MountJournal:GetChildren()}) do
-			local objType = child:GetObjectType()
-			if objType == "Frame" then
-				if child.staticText and child.uniqueCount then
-					B.StripTextures(child)
-					B.CreateBDFrame(child, .25)
-				end
-			elseif objType == "Button" then
-				if child.texture and child.NormalTexture then
+	if CollectionsJournal.selectedTab == COLLECTIONS_JOURNAL_TAB_INDEX_MOUNTS and not styled then
+		P.WaitFor(function()
+			return _G.MJEMountSpecialButtonToolTip and _G.MJEStatisticsHelpToolTip and _G.MJEDisplayHelpToolTip
+		end, function()
+			for _, child in pairs({_G.MountJournal:GetChildren()}) do
+				local objType = child:GetObjectType()
+				if objType == "Frame" then
+					if child.staticText and child.uniqueCount then
+						B.StripTextures(child)
+						B.CreateBDFrame(child, .25)
+					end
+				elseif objType == "Button" then
+					if child.texture and child.NormalTexture then
+						child:SetPushedTexture(0)
+						child:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
+						B.ReskinIcon(child.texture)
+						child:SetNormalTexture(0)
+					elseif child:GetText() == "!" then
+						B.Reskin(child)
+					elseif child.Icon and child.Icon:GetAtlas() == "QuestLog-icon-setting" then
+						child:DisableDrawLayer("BACKGROUND")
+					end
+				elseif objType == "CheckButton" and child.Icon then
 					child:SetPushedTexture(0)
 					child:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
-					B.ReskinIcon(child.texture)
+					B.ReskinIcon(child.Icon)
 					child:SetNormalTexture(0)
-				elseif child:GetText() == "!" then
-					B.Reskin(child)
-				elseif child.Icon and child.Icon:GetAtlas() == "QuestLog-icon-setting" then
-					child:DisableDrawLayer("BACKGROUND")
+					child:SetCheckedTexture(DB.pushedTex)
 				end
-			elseif objType == "CheckButton" and child.Icon then
-				child:SetPushedTexture(0)
-				child:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
-				B.ReskinIcon(child.Icon)
-				child:SetNormalTexture(0)
-				child:SetCheckedTexture(DB.pushedTex)
 			end
-		end
 
-		local SlotButton = _G.MountJournal.SlotButton
-		if SlotButton then
-			local icon = SlotButton.ItemIcon:GetTexture()
-			B.StripTextures(SlotButton)
-			SlotButton.bg = B.ReskinIcon(SlotButton.ItemIcon)
-			SlotButton.bg:SetInside(nil, 2, 2)
-			SlotButton.ItemIcon:SetTexture(icon)
-			SlotButton.ItemIcon:SetInside(SlotButton.bg)
-			SlotButton:SetPushedTexture(0)
-			SlotButton:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
-			SlotButton:GetHighlightTexture():SetInside(SlotButton.bg)
-		end
-
-		hooksecurefunc("MountJournal_InitMountButton", updateButtonBorder)
-
-		P.ReskinTooltip(_G.MJEMountSpecialButtonToolTip)
-		P.ReskinTooltip(_G.MJEStatisticsHelpToolTip)
-		P.ReskinTooltip(_G.MJEDisplayHelpToolTip)
-
-		local controlFrame = MountJournal.MountDisplay.ModelScene.ControlFrame
-		for _, child in pairs({controlFrame:GetChildren()}) do
-			if child.NormalTexture then
-				child.NormalTexture:SetAlpha(0)
-				child.PushedTexture:SetAlpha(0)
+			local SlotButton = _G.MountJournal.SlotButton
+			if SlotButton then
+				local icon = SlotButton.ItemIcon:GetTexture()
+				B.StripTextures(SlotButton)
+				SlotButton.bg = B.ReskinIcon(SlotButton.ItemIcon)
+				SlotButton.bg:SetInside(nil, 2, 2)
+				SlotButton.ItemIcon:SetTexture(icon)
+				SlotButton.ItemIcon:SetInside(SlotButton.bg)
+				SlotButton:SetPushedTexture(0)
+				SlotButton:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
+				SlotButton:GetHighlightTexture():SetInside(SlotButton.bg)
 			end
-		end
-	end)
 
-	styled = true
+			hooksecurefunc("MountJournal_InitMountButton", updateButtonBorder)
+
+			P.ReskinTooltip(_G.MJEMountSpecialButtonToolTip)
+			P.ReskinTooltip(_G.MJEStatisticsHelpToolTip)
+			P.ReskinTooltip(_G.MJEDisplayHelpToolTip)
+
+			local controlFrame = MountJournal.MountDisplay.ModelScene.ControlFrame
+			for _, child in pairs({controlFrame:GetChildren()}) do
+				if child.NormalTexture then
+					child.NormalTexture:SetAlpha(0)
+					child.PushedTexture:SetAlpha(0)
+				end
+			end
+		end)
+
+		styled = true
+	end
 end
 
 function S:MountJournalEnhanced()
