68c10543cb6b970da1337d1a0a0fa2dc7b5b6a93
Witnesscm <fanmicao@126.com>
2025-02-10 21:10:15 +0800

    fix: modify role icon

--- Modules/UnitFrames/Core.lua
+++ Modules/UnitFrames/Core.lua
@@ -1,8 +1,6 @@
 local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
-local oUF = ns.oUF
 local UF = P:RegisterModule("UnitFrames")
-local NUF = B:GetModule("UnitFrames")
 
 function UF:OnLogin()
 	UF:SetupRoleIcons()
--- Modules/UnitFrames/Role.lua
+++ Modules/UnitFrames/Role.lua
@@ -9,10 +9,20 @@ function UF:Configure_RoleIcon(frame)
 	local role = frame.GroupRoleIndicator
 	local mystyle = frame.mystyle
 	if role and mystyle == "raid" then
-		local enable = UF.db["RolePos"]
+		role.__orig = role.__orig or {
+			width = B:Round(role:GetWidth()),
+			height = B:Round(role:GetHeight()),
+			point = {role:GetPoint()}
+		}
 		role:ClearAllPoints()
-		role:SetPoint(enable and G.Points[UF.db["RolePoint"]] or "TOPRIGHT", frame, enable and UF.db["RoleXOffset"] or 5, enable and UF.db["RoleYOffset"] or 5)
-		role:SetSize(enable and UF.db["RoleSize"] or 12, enable and UF.db["RoleSize"] or 12)
+
+		if UF.db["RolePos"] then
+			role:SetPoint(G.Points[UF.db["RolePoint"]], frame, UF.db["RoleXOffset"], UF.db["RoleYOffset"])
+			role:SetSize(UF.db["RoleSize"], UF.db["RoleSize"])
+		else
+			role:SetPoint(unpack(role.__orig.point))
+			role:SetSize(role.__orig.width, role.__orig.height)
+		end
 	end
 end
 
