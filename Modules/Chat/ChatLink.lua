7fb41331f6e1b2931a39cbf8230dd2a49a9d0ee5
Witnesscm <fanmicao@126.com>
2025-08-02 23:47:53 +0800

    feat: update chat link icon

--- Modules/Chat/ChatLink.lua
+++ Modules/Chat/ChatLink.lua
@@ -12,47 +12,52 @@ local function GetHyperlink(hyperlink, texture)
 	end
 end
 
+local TEXTURE_GETTERS = {
+	spell = C_Spell.GetSpellTexture,
+	enchant = C_Spell.GetSpellTexture,
+	mount = C_Spell.GetSpellTexture,
+	item = C_Item.GetItemIconByID,
+	keystone = C_Item.GetItemIconByID,
+	talent = function(id) return select(3, GetTalentInfoByID(id)) end,
+	talentbuild = function(id) return select(4, GetSpecializationInfoByID(id)) end,
+	pvptal = function(id) return select(3, GetPvpTalentInfoByID(id)) end,
+	achievement = function(id) return select(10, GetAchievementInfo(id)) end,
+	currency = function(id)
+		local info = C_CurrencyInfo.GetCurrencyInfo(id)
+		return info and info.iconFileID
+	end,
+	battlepet = function(id) return select(2, C_PetJournal.GetPetInfoBySpeciesID(id)) end,
+	battlePetAbil = function(id) return select(3, C_PetBattles.GetAbilityInfoByID(id)) end,
+	azessence = function(id)
+		local info = C_AzeriteEssence.GetEssenceInfo(id)
+		return info and info.icon
+	end,
+	conduit = function(id)
+		local spell = C_Soulbinds.GetConduitSpellID(id, 1)
+		return spell and C_Spell.GetSpellTexture(spell)
+	end,
+	transmogappearance = function(id)
+		return select(4, C_TransmogCollection.GetAppearanceSourceInfo(id))
+	end,
+	transmogillusion = function(id)
+		local info = C_TransmogCollection.GetIllusionInfo(id)
+		return info and info.icon
+	end
+}
+
 local cache = {}
 
 local function AddChatIcon(link, linkType, id)
 	if not link then return end
 
-	if cache[link] then return cache[link] end
-
-	local texture
-	if linkType == "spell" or linkType == "enchant" or linkType == "mount" then
-		texture = C_Spell.GetSpellTexture(id)
-	elseif linkType == "item" or linkType == "keystone" then
-		texture = C_Item.GetItemIconByID(id)
-	elseif linkType == "talent" then
-		texture = select(3, GetTalentInfoByID(id))
-	elseif linkType == "pvptal" then
-		texture = select(3, GetPvpTalentInfoByID(id))
-	elseif linkType == "achievement" then
-		texture = select(10, GetAchievementInfo(id))
-	elseif linkType == "currency" then
-		local info = C_CurrencyInfo.GetCurrencyInfo(id)
-		texture = info and info.iconFileID
-	elseif linkType == "battlepet" then
-		texture = select(2, C_PetJournal.GetPetInfoBySpeciesID(id))
-	elseif linkType == "battlePetAbil" then
-		texture = select(3, C_PetBattles.GetAbilityInfoByID(id))
-	elseif linkType == "azessence" then
-		local info = C_AzeriteEssence.GetEssenceInfo(id)
-		texture = info and info.icon
-	elseif linkType == "conduit" then
-		local spell = C_Soulbinds.GetConduitSpellID(id, 1)
-		texture = spell and C_Spell.GetSpellTexture(spell)
-	elseif linkType == "transmogappearance" then
-		texture = select(4, C_TransmogCollection.GetAppearanceSourceInfo(id))
-	elseif linkType == "transmogillusion" then
-		local info = C_TransmogCollection.GetIllusionInfo(id)
-		texture = info and info.icon
+	if not cache[link] then
+		local texture = TEXTURE_GETTERS[linkType] and TEXTURE_GETTERS[linkType](tonumber(id))
+		if texture then
+			cache[link] = GetHyperlink(link, texture)
+		end
 	end
 
-	cache[link] = GetHyperlink(link, texture)
-
-	return cache[link]
+	return cache[link] or link
 end
 
 local function AddTradeIcon(link, id)
@@ -65,11 +70,24 @@ local function AddTradeIcon(link, id)
 	return cache[link]
 end
 
+local function AddJournalIcon(link, id)
+	if not link then return end
+
+	if not cache[link] then
+		local info = C_EncounterJournal.GetSectionInfo(id)
+		local texture = info and info.abilityIcon
+		cache[link] = GetHyperlink(link, texture)
+	end
+
+	return cache[link]
+end
+
 function CH:ChatLinkfilter(_, msg, ...)
 	if CH.db["Icon"] then
 		msg = gsub(msg, "(|c%x%x%x%x%x%x%x%x|H(%a+):(%d+).-|h.-|h.-|r)", AddChatIcon)
 		msg = gsub(msg, "(|cnIQ%d:|H(%a+):(%d+).-|h.-|h.-|r)", AddChatIcon)
 		msg = gsub(msg, "(|c%x%x%x%x%x%x%x%x|Htrade:[^:]-:(%d+).-|h.-|h|r)", AddTradeIcon)
+		msg = gsub(msg, "(|c%x%x%x%x%x%x%x%x|Hjournal:2:(%d+):.-|h.-|h|r)", AddJournalIcon)
 	end
 
 	return false, msg, ...
