181dd6bebfd96d792dcd6daf6b289676cd95b80d
Witnesscm <fanmicao@126.com>
2025-11-21 00:39:47 +0800

    5.5.3

--- Core/Changelog.lua
+++ Core/Changelog.lua
@@ -2,6 +2,14 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 
 P.Changelog = [[
+v5.5.3
+[单位框体] 修复3D头像渐隐
+[美化] 添加 BtWQuests 美化
+[美化] 添加 MountJournalEnhanced  美化
+[美化] 添加 Journalator 美化
+[美化] 添加 Collectionator 美化
+[美化] 更新部分插件美化
+
 v5.5.2
 [主要] 更新版本号至5.5.2
 [美化] 更新部分插件美化
